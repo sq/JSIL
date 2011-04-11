89243c63a0c8beb92ff0eaa85a3519feba7708b9
Kevin Gadd <kevin.gadd@gmail.com>
2011-04-10 22:30:11 -0700

    Added another test case for dynamic property usage and handling of temporary locals

---
+++ Tests/SimpleTestCases/DynamicPropertyGetAndCall.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public static class Program {
+    public static void Main (string[] args) {
+        dynamic instance = new CustomType();
+        Console.WriteLine("A = {0}, B = {1}", instance.A, instance.B);
+    }
+}
+
+public class CustomType {
+    public int A = 1;
+    public int B {
+        get;
+        set;
+    }
+    
+    public CustomType () {
+        B = 2;
+    }
+}
