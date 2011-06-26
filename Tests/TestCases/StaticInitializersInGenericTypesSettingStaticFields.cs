149fcb5406075872013519a5c51fd79722e646cf
Rob Ashton <robashton@codeofrob.com>
2011-06-26 14:39:01 +0200

    Test case for static initializers on generic types setting fields

---
+++ Tests/TestCases/StaticInitializersInGenericTypesSettingStaticFields.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        var value = SomeGenericClass<int>.InitializedValue;
+        Console.WriteLine("Woo, found a value of {0}", value);
+    }
+}
+
+public class SomeGenericClass<T>
+{
+    public static readonly int InitializedValue;
+
+    static SomeGenericClass()
+    {
+        InitializedValue = 100;
+    }
+}
