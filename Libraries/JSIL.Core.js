897c4a738c24b905960ca4274c9a7051bb36c4c5
Kevin Gadd <kevin.gadd@gmail.com>
2011-12-03 07:46:58 -0800

    Split printStackTrace library out into its own JS file and only try to use it for diagnostic purposes if it is present. This allows you to opt into using it when needed by including it into the page(s) hosting your code.

--- Libraries/JSIL.Core.js
+++ Libraries/JSIL.Core.js
@@ -28,213 +28,6 @@ if (typeof (Function.prototype.bind) !== "function") {
   };
 }
 
-
-// Domain Public by Eric Wendelin http://eriwen.com/ (2008)
-// Luke Smith http://lucassmith.name/ (2008)
-// Loic Dachary <loic@dachary.org> (2008)
-// Johan Euphrosine <proppy@aminche.com> (2008)
-// Oyvind Sean Kinsey http://kinsey.no/blog (2010)
-// Victor Homyakov <victor-homyakov@users.sourceforge.net> (2010)
-
-/**
-* Main function giving a function stack trace with a forced or passed in Error
-*
-* @cfg {Error} e The error to create a stacktrace from (optional)
-* @cfg {Boolean} guess If we should try to resolve the names of anonymous functions
-* @return {Array} of Strings with functions, lines, files, and arguments where possible
-*/
-function printStackTrace(options) {
-    options = options || {guess: false};
-    var ex = options.e || null;
-    var p = new printStackTrace.implementation(), result = p.run(ex);
-    return result;
-}
-
-printStackTrace.implementation = function() {
-};
-
-printStackTrace.implementation.prototype = {
-    run: function(ex) {
-        ex = ex || this.createException();
-        // Do not use the stored mode: different exceptions in Chrome
-        // may or may not have arguments or stack
-        var mode = this.mode(ex);
-        // Use either the stored mode, or resolve it
-        //var mode = this._mode || this.mode(ex);
-        if (mode === 'other') {
-            return "unknown";
-        } else {
-            return this[mode](ex);
-        }
-    },
-
-    createException: function() {
-        try {
-            this.undef();
-            return null;
-        } catch (e) {
-            return e;
-        }
-    },
-
-    /**
-* @return {String} mode of operation for the environment in question.
-*/
-    mode: function(e) {
-        if (e['arguments'] && e.stack) {
-            return (this._mode = 'chrome');
-        } else if (e.message && typeof window !== 'undefined' && window.opera) {
-            return (this._mode = e.stacktrace ? 'opera10' : 'opera');
-        } else if (e.stack) {
-            return (this._mode = 'firefox');
-        }
-        return (this._mode = 'other');
-    },
-
-    /**
-* Given a context, function name, and callback function, overwrite it so that it calls
-* printStackTrace() first with a callback and then runs the rest of the body.
-*
-* @param {Object} context of execution (e.g. window)
-* @param {String} functionName to instrument
-* @param {Function} function to call with a stack trace on invocation
-*/
-    instrumentFunction: function(context, functionName, callback) {
-        context = context || window;
-        var original = context[functionName];
-        context[functionName] = function instrumented() {
-            callback.call(this, printStackTrace().slice(4));
-            return context[functionName]._instrumented.apply(this, arguments);
-        };
-        context[functionName]._instrumented = original;
-    },
-
-    /**
-* Given a context and function name of a function that has been
-* instrumented, revert the function to it's original (non-instrumented)
-* state.
-*
-* @param {Object} context of execution (e.g. window)
-* @param {String} functionName to de-instrument
-*/
-    deinstrumentFunction: function(context, functionName) {
-        if (context[functionName].constructor === Function &&
-                context[functionName]._instrumented &&
-                context[functionName]._instrumented.constructor === Function) {
-            context[functionName] = context[functionName]._instrumented;
-        }
-    },
-
-    /**
-* Given an Error object, return a formatted Array based on Chrome's stack string.
-*
-* @param e - Error object to inspect
-* @return Array<String> of function calls, files and line numbers
-*/
-    chrome: function(e) {
-        var stack = (e.stack + '\n').replace(/^\S[^\(]+?[\n$]/gm, '').
-          replace(/^\s+at\s+/gm, '').
-          replace(/^([^\(]+?)([\n$])/gm, '{anonymous}()@$1$2').
-          replace(/^Object.<anonymous>\s*\(([^\)]+)\)/gm, '{anonymous}()@$1').split('\n');
-        stack.pop();
-        return stack;
-    },
-
-    /**
-* Given an Error object, return a formatted Array based on Firefox's stack string.
-*
-* @param e - Error object to inspect
-* @return Array<String> of function calls, files and line numbers
-*/
-    firefox: function(e) {
-        return e.stack.replace(/(?:\n@:0)?\s+$/m, '').replace(/^\(/gm, '{anonymous}(').split('\n');
-    },
-
-    /**
-* Given an Error object, return a formatted Array based on Opera 10's stacktrace string.
-*
-* @param e - Error object to inspect
-* @return Array<String> of function calls, files and line numbers
-*/
-    opera10: function(e) {
-        var stack = e.stacktrace;
-        var lines = stack.split('\n'), ANON = '{anonymous}', lineRE = /.*line (\d+), column (\d+) in ((<anonymous function\:?\s*(\S+))|([^\(]+)\([^\)]*\))(?: in )?(.*)\s*$/i, i, j, len;
-        for (i = 2, j = 0, len = lines.length; i < len - 2; i++) {
-            if (lineRE.test(lines[i])) {
-                var location = RegExp.$6 + ':' + RegExp.$1 + ':' + RegExp.$2;
-                var fnName = RegExp.$3;
-                fnName = fnName.replace(/<anonymous function\:?\s?(\S+)?>/g, ANON);
-                lines[j++] = fnName + '@' + location;
-            }
-        }
-
-        lines.splice(j, lines.length - j);
-        return lines;
-    },
-
-    // Opera 7.x-9.x only!
-    opera: function(e) {
-        var lines = e.message.split('\n'), ANON = '{anonymous}', lineRE = /Line\s+(\d+).*script\s+(http\S+)(?:.*in\s+function\s+(\S+))?/i, i, j, len;
-
-        for (i = 4, j = 0, len = lines.length; i < len; i += 2) {
-            //TODO: RegExp.exec() would probably be cleaner here
-            if (lineRE.test(lines[i])) {
-                lines[j++] = (RegExp.$3 ? RegExp.$3 + '()@' + RegExp.$2 + RegExp.$1 : ANON + '()@' + RegExp.$2 + ':' + RegExp.$1) + ' -- ' + lines[i + 1].replace(/^\s+/, '');
-            }
-        }
-
-        lines.splice(j, lines.length - j);
-        return lines;
-    },
-
-    // Safari, IE, and others
-    other: function(curr) {
-        var ANON = '{anonymous}', fnRE = /function\s*([\w\-$]+)?\s*\(/i, stack = [], fn, args, maxStackSize = 10;
-        while (curr && stack.length < maxStackSize) {
-            fn = fnRE.test(curr.toString()) ? RegExp.$1 || ANON : ANON;
-            args = Array.prototype.slice.call(curr['arguments'] || []);
-            stack[stack.length] = fn + '(' + this.stringifyArguments(args) + ')';
-            curr = curr.caller;
-        }
-        return stack;
-    },
-
-    /**
-* Given arguments array as a String, subsituting type names for non-string types.
-*
-* @param {Arguments} object
-* @return {Array} of Strings with stringified arguments
-*/
-    stringifyArguments: function(args) {
-        var result = [];
-        var slice = Array.prototype.slice;
-        for (var i = 0; i < args.length; ++i) {
-            var arg = args[i];
-            if (arg === undefined) {
-                result[i] = 'undefined';
-            } else if (arg === null) {
-                result[i] = 'null';
-            } else if (arg.constructor) {
-                if (arg.constructor === Array) {
-                    if (arg.length < 3) {
-                        result[i] = '[' + this.stringifyArguments(arg) + ']';
-                    } else {
-                        result[i] = '[' + this.stringifyArguments(slice.call(arg, 0, 1)) + '...' + this.stringifyArguments(slice.call(arg, -1)) + ']';
-                    }
-                } else if (arg.constructor === Object) {
-                    result[i] = '#object';
-                } else if (arg.constructor === Function) {
-                    result[i] = '#function';
-                } else if (arg.constructor === String) {
-                    result[i] = '"' + arg + '"';
-                }
-            }
-        }
-        return result.join(',');
-    }
-};
-
-
 JSIL.GlobalNamespace = this;
 
 JSIL.PrivateNamespaces = {};
@@ -817,7 +610,7 @@ JSIL.RegisterName = function (name, privateNamespace, isPublic, creator, initial
   var localName = privateName.localName;
 
   if (privateName.exists()) {
-    JSIL.DuplicateDefinitionWarning(name, false, privateName.get().__CallStack__, privateNamespace);
+    JSIL.DuplicateDefinitionWarning(name, false, privateName.get().__CallStack__ || null, privateNamespace);
     return;
   }
 
@@ -1254,7 +1047,7 @@ JSIL.DuplicateDefinitionWarning = function (fullName, isPublic, definedWhere, in
   if (inAssembly)
     message += " in assembly '" + inAssembly + "'";
 
-  if (definedWhere) {
+  if (definedWhere && (definedWhere !== null)) {
     message += ".\r\nPreviously defined at:\r\n  ";
     message += definedWhere.join("\r\n  ");
   }
@@ -1316,7 +1109,6 @@ JSIL.MakeStaticClass = function (fullName, isPublic, genericArguments, initializ
 
   var assembly = $private;
   var localName = JSIL.GetLocalName(fullName);
-  var stack = printStackTrace();
 
   var typeObject = JSIL.CloneObject(System.RuntimeType);
   typeObject.GetType = function () {
@@ -1324,7 +1116,10 @@ JSIL.MakeStaticClass = function (fullName, isPublic, genericArguments, initializ
   };
   typeObject.FullName = typeObject.__FullName__ = fullName;
   typeObject.__TypeId__ = ++JSIL.$NextTypeId;
-  typeObject.__CallStack__ = stack;
+
+  if (typeof (printStackTrace) === "function")
+    typeObject.__CallStack__ = printStackTrace();
+
   typeObject.__ShortName__ = localName;
   typeObject.__IsStatic__ = true;
   typeObject.__Initializers__ = [];
@@ -1370,7 +1165,9 @@ JSIL.MakeType = function (baseType, fullName, isReferenceType, isPublic, generic
   var assembly = $private;
   var localName = JSIL.GetLocalName(fullName);
 
-  var stack = printStackTrace();
+  var stack = null;
+  if (typeof (printStackTrace) === "function")
+    stack = printStackTrace();
 
   var createTypeObject = function () {
     var typeObject = function () {
@@ -1391,7 +1188,6 @@ JSIL.MakeType = function (baseType, fullName, isReferenceType, isPublic, generic
     };
 
     typeObject.__TypeId__ = ++JSIL.$NextTypeId;
-    typeObject.__CallStack__ = stack;
     typeObject.__IsArray__ = false;
     typeObject.__Initializers__ = [];
     typeObject.__TypeInitialized__ = false;
@@ -1402,6 +1198,9 @@ JSIL.MakeType = function (baseType, fullName, isReferenceType, isPublic, generic
     typeObject.FullName = typeObject.__FullName__ = fullName;
     typeObject.__ShortName__ = localName;
     typeObject.__LockCount__ = 0;
+
+    if (stack !== null)
+      typeObject.__CallStack__ = stack;
 
     typeObject.__GenericArguments__ = genericArguments || [];
     if (typeObject.__GenericArguments__.length > 0) {
@@ -1465,13 +1264,11 @@ JSIL.MakeStruct = function (baseType, fullName, isPublic, genericArguments, init
 
 JSIL.MakeInterface = function (fullName, isPublic, genericArguments, members) {
   var localName = JSIL.GetLocalName(fullName);
-  var stack = printStackTrace();
 
   var typeObject = function() {
     throw new Error("Cannot construct an instance of an interface");
   };
   typeObject.__TypeId__ = ++JSIL.$NextTypeId;
-  typeObject.__CallStack__ = stack;
   typeObject.__Members__ = members;
   typeObject.__ShortName__ = localName;
   typeObject.FullName = typeObject.__FullName__ = fullName;
@@ -1481,6 +1278,9 @@ JSIL.MakeInterface = function (fullName, isPublic, genericArguments, members) {
     return typeObject;
   };
   typeObject.prototype = JSIL.CloneObject(JSIL.Interface.prototype);
+
+  if (typeof (printStackTrace) === "function")
+    typeObject.__CallStack__ = printStackTrace();
 
   JSIL.RegisterName(fullName, $private, isPublic, function () { return typeObject; });
 };
@@ -1508,7 +1308,6 @@ JSIL.MakeEnumValue = function (enumType, value, key) {
 
 JSIL.MakeEnum = function (fullName, isPublic, members, isFlagsEnum) {
   var localName = JSIL.GetLocalName(fullName);
-  var stack = printStackTrace();
   
   var enumType = System.Enum;
   var prototype = JSIL.CloneObject(enumType.prototype);
@@ -1518,7 +1317,6 @@ JSIL.MakeEnum = function (fullName, isPublic, members, isFlagsEnum) {
 
   var result = {
     prototype: prototype,
-    __CallStack__: stack,
     __BaseType__: enumType,
     __FullName__: fullName, 
     FullName: fullName,
@@ -1528,6 +1326,9 @@ JSIL.MakeEnum = function (fullName, isPublic, members, isFlagsEnum) {
     __IsFlagsEnum__: isFlagsEnum,
     __ValueToName__: {}
   };
+
+  if (typeof (printStackTrace) === "function")
+    result.__CallStack__ = printStackTrace();
 
   result.toString = function () {
     return localName;
