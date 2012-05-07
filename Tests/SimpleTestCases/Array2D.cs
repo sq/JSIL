46fdffb172bfb502c6a96f757707d3fdcaee976f
matra774 <matej.trampus@gmail.com>
2012-05-07 22:40:19 +0200

    Added test for 2d array

---
+++ Tests/SimpleTestCases/Array2D.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public static class Program
+{
+  public static void Main(string[] args) {
+      
+      var x = new int[,] { { 1, 2, 3 }, { 4, 5, 6 } };
+
+      Console.WriteLine(x.Length);
+
+      for (int i = 0; i < x.GetLength(0);i++ )
+      {
+        for (int j = 0; j < x.GetLength(1); j++)
+        {
+          Console.WriteLine(x[i,j]);
+        }
+
+      }
+
+    }
+}
