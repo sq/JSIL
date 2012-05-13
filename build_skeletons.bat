00907ccaa904c5b45abebaecbc740dded36625fe
Virgile Bello <virgile.bello@gmail.com>
2012-05-13 21:35:20 +0900

    Build Skeletons batch file: use relative path.

--- build_skeletons.bat
+++ build_skeletons.bat
@@ -1,3 +1,3 @@
 pushd bin
-JSILc "Microsoft.Xna.Framework.Game, Version=4.0.0.0, Culture=neutral, PublicKeyToken=842cf8be1de50553" "Microsoft.Xna.Framework.Xact, Version=4.0.0.0, Culture=neutral, PublicKeyToken=842cf8be1de50553" "C:\Users\Kevin\Documents\Projects\JSIL\Skeletons\skeletons.jsilconfig"
+JSILc "Microsoft.Xna.Framework.Game, Version=4.0.0.0, Culture=neutral, PublicKeyToken=842cf8be1de50553" "Microsoft.Xna.Framework.Xact, Version=4.0.0.0, Culture=neutral, PublicKeyToken=842cf8be1de50553" "..\Skeletons\skeletons.jsilconfig"
 popd
