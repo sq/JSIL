49ba0b01b9d0aabc9e843b9bd4f62a5858ea4d7d
Kevin Gadd <kevin.gadd@gmail.com>
2012-06-05 05:16:22 -0700

    Some work on actual file storage Allow specifying the path to the root directory Replace backslashes with forward slashes in paths

--- Libraries/JSIL.XNAStorage.js
+++ Libraries/JSIL.XNAStorage.js
@@ -8,136 +8,163 @@ if (typeof ($jsilxna) === "undefined")
 
 JSIL.MakeClass($jsilcore.System.Object, "VirtualVolume", true, [], function ($) {
   $.RawMethod(false, ".ctor", function (name, inodes) {
-  	this.inodes = [];
-  	this.name = name;
+    this.inodes = [];
+    this.name = name;
 
-  	if (inodes && (inodes.length > 0)) {
-  		this.initFromInodes(inodes);
-  	} else {
-  		this.rootDirectory = new VirtualDirectory(
-  			this, null, this.makeInode(null, "directory", "/")
-		);
-  	}
+    if (JSIL.IsArray(inodes)) {
+      this.initFromInodes(inodes);
+    } else {
+      var rootPath = "/";
+
+      if (typeof (inodes) === "string") {
+        rootPath = this.normalizePath(inodes);
+
+        if (rootPath[rootPath.length - 1] !== "/")
+          rootPath += "/";
+      }
+
+      this.rootDirectory = new VirtualDirectory(
+        this, null, this.makeInode(null, "directory", rootPath)
+      );
+    }
   });
 
   $.RawMethod(false, "initFromInodes", function (inodes) {
-	// Create local copies of all the source inodes
+    // Create local copies of all the source inodes
 
-  	for (var i = 0, l = inodes.length; i < l; i++) {
-  		var inode = inodes[i], resultInode;
+    for (var i = 0, l = inodes.length; i < l; i++) {
+      var inode = inodes[i], resultInode;
 
-  		if (typeof (inode.parent) === "number") {
-  			resultInode = this.makeInode(this.inodes[inode.parent], inode.type, inode.name);
-  		} else {
-  			resultInode = this.makeInode(null, inode.type, inode.name);
-  		}
+      if (typeof (inode.parent) === "number") {
+        resultInode = this.makeInode(this.inodes[inode.parent], inode.type, inode.name);
+      } else {
+        resultInode = this.makeInode(null, inode.type, inode.name);
+      }
 
-		if (inode.metadata)
-  			resultInode.metadata = inode.metadata;
+      if (inode.metadata)
+        resultInode.metadata = inode.metadata;
 
-  		if (inode.ref)
-  			resultInode.ref = inode.ref;
-  	}
+      if (inode.blobName)
+        resultInode.blobName = inode.blobName;
+    }
 
-  	this.rootDirectory = new VirtualDirectory(
-  		this, null, this.inodes[0]
-	);
+    this.rootDirectory = new VirtualDirectory(
+      this, null, this.inodes[0]
+    );
 
-  	for (var i = 1, l = inodes.length; i < l; i++) {
-  		var inode = this.inodes[i];
-  		var parentInode = this.inodes[inode.parent];
+    for (var i = 1, l = inodes.length; i < l; i++) {
+      var inode = this.inodes[i];
+      var parentInode = this.inodes[inode.parent];
 
-  		switch (inode.type) {
-  			case "directory":
-  				new VirtualDirectory(
-  					this, parentInode.object, inode
-				);
+      switch (inode.type) {
+        case "directory":
+          new VirtualDirectory(
+              this, parentInode.object, inode
+          );
 
-  				break;
+          break;
 
-  			case "file":
-  				new VirtualFile(
-  					parentInode.object, inode
-				);
+        case "file":
+          new VirtualFile(
+              parentInode.object, inode
+          );
 
-  				break;
-  		}
-  	}
+          break;
+      }
+    }
   });
 
   $.RawMethod(false, "makeInode", function (parent, type, name) {
-  	var inode = {
-		type: type,
-		name: name
-  	};
+    var inode = {
+      type: type,
+      name: name
+    };
 
-  	if (parent) {
-	  	Object.defineProperty(
-	  		inode, "parent", {
-	  			value: parent.index,
-	  			configurable: false,
-	  			enumerable: true,
-	  			writable: false
-	  		}
-		);
-  	}
+    if (parent) {
+      Object.defineProperty(
+        inode, "parent", {
+          value: parent.index,
+          configurable: false,
+          enumerable: true,
+          writable: false
+        }
+      );
+    }
 
-  	Object.defineProperty(
-  		inode, "index", {
-  			value: this.inodes.length,
-  			configurable: false,
-  			enumerable: false,
-  			writable: false
-  		}
-	);
+    Object.defineProperty(
+      inode, "index", {
+        value: this.inodes.length,
+        configurable: false,
+        enumerable: false,
+        writable: false
+      }
+    );
 
-  	this.inodes.push(inode);
+    this.inodes.push(inode);
 
-  	return inode;
+    return inode;
   });
 
-  $.RawMethod(false, "stripRoot", function (path) {
-  	var indexOfRoot = path.indexOf(this.rootDirectory.path);
-  	if (indexOfRoot === 0)
-  		path = path.substr(this.rootDirectory.path.length);
+  $.RawMethod(false, "normalizePath", function (path) {
+    var backslashRe = /\\/g;
 
-  	return path;
+    path = path.replace(backslashRe, "/");
+
+    if (this.rootDirectory) {
+      var indexOfRoot = path.indexOf(this.rootDirectory.path);
+      if (indexOfRoot === 0)
+        path = path.substr(this.rootDirectory.path.length);
+    }
+
+    return path;
   });  
 
   $.RawMethod(false, "createDirectory", function (path) {
-  	path = this.stripRoot(path);
+    path = this.normalizePath(path);
 
-  	var pieces = path.split("/");
+    var pieces = path.split("/");
 
-  	for (var i = 0, l = pieces.length; i < l; i++) {
-  		var containingPath = pieces.slice(0, i).join("/") + "/";
+    for (var i = 0, l = pieces.length; i < l; i++) {
+      var containingPath = pieces.slice(0, i).join("/") + "/";
 
-  		var containingDirectory = this.rootDirectory.resolvePath(containingPath);
-  		containingDirectory.createDirectory(pieces[i], true);
-  	}
+      var containingDirectory = this.rootDirectory.resolvePath(containingPath);
+      containingDirectory.createDirectory(pieces[i], true);
+    }
 
-  	return this.rootDirectory.resolvePath(path);
+    return this.rootDirectory.resolvePath(path);
   });
 
   $.RawMethod(false, "createFile", function (path, allowExisting) {
-  	path = this.stripRoot(path);
+    path = this.normalizePath(path);
 
-  	var lastSlash = path.lastIndexOf("/"), parentDirectory, fileName;
-  	if (lastSlash >= 0) {
-  		parentDirectory = this.rootDirectory.resolvePath(path.substr(0, lastSlash), true);
-  		fileName = path.substr(lastSlash + 1);
-  	} else {
-  		parentDirectory = this.rootDirectory;
-  		fileName = path;
-  	}
+    var lastSlash = path.lastIndexOf("/"), parentDirectory, fileName;
+    if (lastSlash >= 0) {
+      parentDirectory = this.rootDirectory.resolvePath(path.substr(0, lastSlash), true);
+      fileName = path.substr(lastSlash + 1);
+    } else {
+      parentDirectory = this.rootDirectory;
+      fileName = path;
+    }
 
-	return parentDirectory.createFile(fileName, allowExisting);
+    return parentDirectory.createFile(fileName, allowExisting);
   });
 
   $.RawMethod(false, "resolvePath", function (path, throwOnFail) {
-  	path = this.stripRoot(path);
+    path = this.normalizePath(path);
 
-  	return this.rootDirectory.resolvePath(path, throwOnFail);
+    return this.rootDirectory.resolvePath(path, throwOnFail);
+  });
+
+  $.RawMethod(false, "flush", function () {    
+    throw new Error("Not implemented");
+  });
+
+  $.RawMethod(false, "getBlob", function (name) {
+    throw new Error("Not implemented");
+  });
+
+  $.RawMethod(false, "setBlob", function (name, value) {    
+    throw new Error("Not implemented");
   });
 
   $.RawMethod(false, "toString", function () {
@@ -147,144 +174,146 @@ JSIL.MakeClass($jsilcore.System.Object, "VirtualVolume", true, [], function ($) 
 
 JSIL.MakeClass($jsilcore.System.Object, "VirtualDirectory", true, [], function ($) {
   $.RawMethod(false, ".ctor", function (volume, parent, inode) {
-  	if (inode.type !== "directory")
-  		throw new Error("Inode is not a directory");
+    if (inode.type !== "directory")
+      throw new Error("Inode is not a directory");
 
-  	this.volume = volume;
- 	this.parent = parent;
-  	this.inode = inode;
+    this.volume = volume;
+    this.parent = parent;
+    this.inode = inode;
 
-  	JSIL.SetValueProperty(
-  		this, "directories", {}
-	);
-	JSIL.SetValueProperty(
-		this, "files", {}
-	);
+    JSIL.SetValueProperty(
+      this, "directories", {}
+    );
+    JSIL.SetValueProperty(
+      this, "files", {}
+    );
 
-  	Object.defineProperty(
-  		this, "name", {
-  			configurable: false,
-  			get: function () {
-  				return inode.name;
-  			}
-  		}
-	);
+    Object.defineProperty(
+      this, "name", {
+        configurable: false,
+        get: function () {
+          return inode.name;
+        }
+      }
+    );
 
-	JSIL.SetValueProperty(
-		this, "path", 
-		parent ? parent.path + (this.name + "/") : "/"
-	);
+    JSIL.SetValueProperty(
+      this, "path", 
+      parent ? 
+        parent.path + (this.name + "/") : 
+        this.name
+    );
 
-	Object.defineProperty(
-		this.inode, "object", {
-			value: this,
-			enumerable: false,
-			configurable: false,
-			writable: false
-		}
-	);
+    Object.defineProperty(
+      this.inode, "object", {
+        value: this,
+        enumerable: false,
+        configurable: false,
+        writable: false
+      }
+    );
 
-  	if (parent)
-  		parent.directories[this.name.toLowerCase()] = this;
+    if (parent)
+      parent.directories[this.name.toLowerCase()] = this;
   });
 
   $.RawMethod(false, "getFile", function (name) {
-  	var file = this.files[name.toLowerCase()];
-  	if (!file)
-  		return null;
+    var file = this.files[name.toLowerCase()];
+    if (!file)
+      return null;
 
-  	return file;
+    return file;
   });
 
   $.RawMethod(false, "getDirectory", function (name) {
-  	if ((name === ".") || (name === ""))
-  		return this;
-  	else if (name === "..")
-  		return this.parent;
+    if ((name === ".") || (name === ""))
+      return this;
+    else if (name === "..")
+      return this.parent;
 
-  	var directory = this.directories[name.toLowerCase()];
-  	if (!directory)
-  		return null;
+    var directory = this.directories[name.toLowerCase()];
+    if (!directory)
+      return null;
 
-  	return directory;
+    return directory;
   });
 
   $.RawMethod(false, "createFile", function (name, allowExisting) {
-  	var existingFile = this.getFile(name);
-  	if (existingFile) {
-  		if (allowExisting)
-  			return existingFile;
-  		else
-  			throw new Error("A file named '" + name + "' already exists.");
-  	}
+    var existingFile = this.getFile(name);
+    if (existingFile) {
+      if (allowExisting)
+        return existingFile;
+      else
+        throw new Error("A file named '" + name + "' already exists.");
+    }
 
-  	return new VirtualFile(
-  		this, this.volume.makeInode(this.inode, "file", name)
-	);
+    return new VirtualFile(
+      this, this.volume.makeInode(this.inode, "file", name)
+    );
   });
 
   $.RawMethod(false, "createDirectory", function (name, allowExisting) {
-  	var existingDirectory = this.getDirectory(name);
-  	if (existingDirectory) {
-  		if (allowExisting)
-  			return existingDirectory;
-  		else
-  			throw new Error("A directory named '" + name + "' already exists.");
-  	}
+    var existingDirectory = this.getDirectory(name);
+    if (existingDirectory) {
+      if (allowExisting)
+        return existingDirectory;
+      else
+        throw new Error("A directory named '" + name + "' already exists.");
+    }
 
-  	return new VirtualDirectory(
-  		this.volume, this, this.volume.makeInode(this.inode, "directory", name)
-	);
+    return new VirtualDirectory(
+      this.volume, this, this.volume.makeInode(this.inode, "directory", name)
+    );
   });
 
   $.RawMethod(false, "resolvePath", function (path, throwOnFail) {
-  	var firstSlash = path.indexOf("/"), itemName, childPath;
+    var firstSlash = path.indexOf("/"), itemName, childPath;
 
-  	if (typeof(throwOnFail) === "undefined")
-  		throwOnFail = true;
+    if (typeof(throwOnFail) === "undefined")
+      throwOnFail = true;
 
-  	if (firstSlash >= 0) {
-  		itemName = path.substr(0, firstSlash);
-  		childPath = path.substr(firstSlash);
-  	} else {
-  		itemName = path;
-  		childPath = null;
-  	}
+    if (firstSlash >= 0) {
+      itemName = path.substr(0, firstSlash);
+      childPath = path.substr(firstSlash);
+    } else {
+      itemName = path;
+      childPath = null;
+    }
 
-  	var forceDirectory = childPath === "/";
-  	if (forceDirectory) {
-  		childPath = null;
-  	} else if (childPath) {
-  		while (childPath[0] === "/")
-  			childPath = childPath.substr(1);
-  	}
+    var forceDirectory = childPath === "/";
+    if (forceDirectory) {
+      childPath = null;
+    } else if (childPath) {
+      while (childPath[0] === "/")
+        childPath = childPath.substr(1);
+    }
 
-  	var result = this.getDirectory(itemName);
-  	if (result === null) {
-  		if (forceDirectory) {
-  			if (throwOnFail)
-  				throw new Error("No directory named '" + itemName + "' could be found in directory '" + this.path + "'.");
-  			else
-  				result = null;
-		} else
-  			result = this.getFile(itemName);
-  	}
+    var result = this.getDirectory(itemName);
+    if (result === null) {
+      if (forceDirectory) {
+        if (throwOnFail)
+          throw new Error("No directory named '" + itemName + "' could be found in directory '" + this.path + "'.");
+        else
+          result = null;
+      } else
+        result = this.getFile(itemName);
+    }
 
-  	if (childPath) {
-  		if (result) {
-  			return result.resolvePath(childPath, throwOnFail);
-  		} else {
-  			if (throwOnFail)
-  				throw new Error("No directory named '" + itemName + "' could be found in directory '" + this.path + "'.");
-  			else
-  				return null;
-  		}
-  	} else {
-  		if (!result && throwOnFail)
-			throw new Error("No file or directory named '" + itemName + "' could be found in directory '" + this.path + "'.");
+    if (childPath) {
+      if (result) {
+        return result.resolvePath(childPath, throwOnFail);
+      } else {
+        if (throwOnFail)
+          throw new Error("No directory named '" + itemName + "' could be found in directory '" + this.path + "'.");
+        else
+          return null;
+      }
+    } else {
+      if (!result && throwOnFail)
+        throw new Error("No file or directory named '" + itemName + "' could be found in directory '" + this.path + "'.");
 
-  		return result;
-  	}
+      return result;
+    }
   });
 
   $.RawMethod(false, "toString", function () {
@@ -294,39 +323,49 @@ JSIL.MakeClass($jsilcore.System.Object, "VirtualDirectory", true, [], function (
 
 JSIL.MakeClass($jsilcore.System.Object, "VirtualFile", true, [], function ($) {
   $.RawMethod(false, ".ctor", function (parent, inode) {
-  	if (inode.type !== "file")
-  		throw new Error("Inode is not a file");
+    if (inode.type !== "file")
+      throw new Error("Inode is not a file");
 
- 	this.parent = parent;
-  	this.inode = inode;
+    this.parent = parent;
+    this.inode = inode;
 
-  	Object.defineProperty(
-  		this, "name", {
-  			configurable: false,
-  			get: function () {
-  				return inode.name;
-  			}
-  		}
-	);
+    Object.defineProperty(
+      this, "name", {
+        configurable: false,
+        get: function () {
+          return inode.name;
+        }
+      }
+    );
 
-	JSIL.SetValueProperty(
-		this, "path", parent.path + this.name
-	);
+    JSIL.SetValueProperty(
+      this, "path", parent.path + this.name
+    );
 
-	Object.defineProperty(
-		this.inode, "object", {
-			value: this,
-			enumerable: false,
-			configurable: false,
-			writable: false
-		}
-	);
+    Object.defineProperty(
+      this.inode, "object", {
+        value: this,
+        enumerable: false,
+        configurable: false,
+        writable: false
+      }
+    );
 
-	parent.files[this.name.toLowerCase()] = this;
+    Object.defineProperty(
+      this, "volume", {
+        get: function () {
+          return this.parent.volume;
+        },
+        enumerable: false,
+        configurable: false
+      }
+    );
+
+    parent.files[this.name.toLowerCase()] = this;
   });
 
   $.RawMethod(false, "toString", function () {
-    return "<Virtual File '" + this.path + "' in volume '" + this.parent.volume.name + "'>";
+    return "<Virtual File '" + this.path + "' in volume '" + this.volume.name + "'>";
   });
 });
 
@@ -338,7 +377,9 @@ JSIL.ImplementExternals("Microsoft.Xna.Framework.Storage.StorageContainer", func
           $.String
         ], [])), 
     function _ctor (device, index, displayName) {
-      // FIXME
+      this.device = device;
+      this.index = index;
+      this.displayName = displayName;
     }
   );
 
