4cad4961391bdd575e5db7afca912e0c8a978941
Kevin Gadd <kevin.gadd@gmail.com>
2012-06-16 00:04:14 -0700

    Check in Game Type files

--- build_demos.bat
+++ build_demos.bat
@@ -9,4 +9,5 @@ JSILc "C:\Users\Kevin\Documents\Projects\RPGStarterKit\RolePlayingGameWindows.sl
 JSILc "C:\Users\Kevin\Documents\Projects\lumberjack\LumberjackPC.sln" "C:\Users\Kevin\Documents\Projects\lumberjack\Lumberjack\Lumberjack\bin\x86\Debug\Lumberjack.XmlSerializers.dll" "C:\Users\Kevin\Documents\Projects\JSIL\jsil.org\demos\Lumberjack\Lumberjack.jsilconfig" --platform:x86 --configuration:Debug
 JSILc "C:\Users\Kevin\Documents\Projects\XNAVERGE\Sully.sln" "C:\Users\Kevin\Documents\Projects\JSIL\jsil.org\demos\Sully\Sully.jsilconfig" --platform:x86 --configuration:Debug
 JSILc "C:\Users\Kevin\Documents\Projects\Soulcaster\SoulcasterHTML5.sln" "C:\Users\Kevin\Documents\Projects\Soulcaster\Soulcaster2\Soulcaster2\bin\x86\DebugPC\TarchonData.XmlSerializers.dll" "C:\Users\Kevin\Documents\Projects\JSIL\jsil.org\demos\Soulcaster\Soulcaster.jsilconfig" --platform:x86 --configuration:DebugPC
+JSILc "C:\Game Design\Game Type\CodeNew\GameType\GameType.sln" "C:\Users\Kevin\Documents\Projects\JSIL\jsil.org\demos\GameType\GameType.jsilconfig" --platform:x86 --configuration:Debug
 popd
