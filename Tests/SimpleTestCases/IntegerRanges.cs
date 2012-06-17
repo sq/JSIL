f138daf88c94be4ae8491d906ad8c547a1025408
Kevin Gadd <kevin.gadd@gmail.com>
2012-06-17 02:29:11 -0700

    Add a test for the integer range constants.

---
+++ Tests/SimpleTestCases/IntegerRanges.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Reflection;
+using JSIL.Meta;
+
+public static class Program {
+    [JSReplacement("eval($typeName)[$constantName]")]
+    public static object GetConstant (string typeName, string constantName) {
+        var theType = Type.GetType(typeName, true);
+        var theConstant = theType.GetField(
+            constantName, 
+            BindingFlags.Public | BindingFlags.Static
+        );
+        return theConstant.GetRawConstantValue();
+    }
+
+    public static void DumpLimits (string typeName) {
+        Console.WriteLine(
+            "{0} {1} {2}", typeName,
+            GetConstant(typeName, "MinValue"),
+            GetConstant(typeName, "MaxValue")
+        );
+    }
+
+    public static void Main (string[] args) {
+        DumpLimits("System.Byte");
+        DumpLimits("System.SByte");
+        DumpLimits("System.UInt16");
+        DumpLimits("System.Int16");
+        DumpLimits("System.UInt32");
+        DumpLimits("System.Int32");
+    }
+}
