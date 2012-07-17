e3975cd7f04f9e88d5a87d8e0ca44103993d3507
Kevin Gadd <kevin.gadd@gmail.com>
2012-07-17 12:28:49 -0700

    Fix a test.

--- Tests/FormattingTests.cs
+++ Tests/FormattingTests.cs
@@ -563,8 +563,8 @@ namespace JSIL.Tests {
                 );
 
                 try {
-                    Assert.IsTrue(generatedJs.Contains("function Any$b1 (TSource, source)"));
-                    Assert.IsTrue(generatedJs.Contains("function Any$b1 (TSource, source, predicate)"));
+                    Assert.IsTrue(generatedJs.Contains("function CommonExtensionMethodsSimple_Any$b1 (TSource, source)"));
+                    Assert.IsTrue(generatedJs.Contains("function CommonExtensionMethodsSimple_Any$b1 (TSource, source, predicate)"));
                 } catch {
                     Console.WriteLine(generatedJs);
 
