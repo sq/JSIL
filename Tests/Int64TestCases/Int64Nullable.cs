23d7a1dda612b5ecf1b2f7161ce0ab706858fe94
Markus Johnsson <markus.johnsson@infviz.com>
2012-09-19 20:48:36 +0200

    Added Int64Nullable

---
+++ Tests/Int64TestCases/Int64Nullable.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        int? x = 1000;
+        long? y = 2000;
+        long? z = null;
+
+        Console.WriteLine(x);
+        Console.WriteLine(y);
+        Console.WriteLine(x + y);
+        Console.WriteLine(z == null ? "True" : "False");
+        Console.WriteLine((x + y) == null ? "True" : "False");
+        Console.WriteLine((x + z) == null ? "True" : "False");
+        Console.WriteLine((z + z) == null ? "True" : "False");
+
+    }
+}
