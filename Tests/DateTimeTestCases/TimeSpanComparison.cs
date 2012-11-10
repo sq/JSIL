fdf5e4f28972f720a7c14eaa73b077571ef58215
Kevin Gadd <kevin.gadd@gmail.com>
2012-11-10 10:26:29 -0800

    Fix TimeSpan comparison operators.

---
+++ Tests/DateTimeTestCases/TimeSpanComparison.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public class Program {
+    public static void Main() {
+        var ticksZero = TimeSpan.FromTicks(0);
+        var secondsZero = TimeSpan.FromSeconds(0);
+        var secondsOne = TimeSpan.FromSeconds(1);
+        var msOneThousand = TimeSpan.FromMilliseconds(1000);
+
+        Console.WriteLine(ticksZero == secondsZero ? 1 : 0);
+        Console.WriteLine(ticksZero != secondsZero ? 1 : 0);
+        Console.WriteLine(ticksZero != secondsOne ? 1 : 0);
+        Console.WriteLine(msOneThousand > ticksZero ? 1 : 0);
+        Console.WriteLine(msOneThousand < ticksZero ? 1 : 0);
+        Console.WriteLine(msOneThousand < secondsOne ? 1 : 0);
+    }
+}
