a4161a884825ecfc17c38654e839159250fc053b
Kevin Gadd <kevin.gadd@gmail.com>
2012-11-13 15:57:25 -0800

    Remove SC1/SC2 demos

--- build_demos.bat
+++ build_demos.bat
@@ -1,6 +1,4 @@
 del /s/q jsil.org\demos\*.js
-bin\JSILc "C:\Users\Kevin\Documents\Projects\Soulcaster\Soulcaster1HTML5.sln" "C:\Users\Kevin\Documents\Projects\Soulcaster\Soulcaster1\Soulcaster1\bin\x86\DebugPC\TarchonData.XmlSerializers.dll" "jsil.org\demos\Soulcaster\Soulcaster.jsilconfig" --platform:x86 --configuration:DebugPC
-bin\JSILc "C:\Users\Kevin\Documents\Projects\Soulcaster\Soulcaster2HTML5.sln" "C:\Users\Kevin\Documents\Projects\Soulcaster\Soulcaster2\Soulcaster2\bin\x86\DebugPC\TarchonData.XmlSerializers.dll" "jsil.org\demos\Soulcaster2\Soulcaster2.jsilconfig" --platform:x86 --configuration:DebugPC
 bin\JSILc "C:\Game Design\Game Type\CodeNew\GameType\GameType.sln" "jsil.org\demos\GameType\GameType.jsilconfig" --platform:x86 --configuration:Debug
 bin\JSILc "C:\Users\Kevin\Documents\Projects\lumberjack\LumberjackPC.sln" "C:\Users\Kevin\Documents\Projects\lumberjack\Lumberjack\Lumberjack\bin\x86\Debug\Lumberjack.XmlSerializers.dll" "jsil.org\demos\Lumberjack\Lumberjack.jsilconfig" --platform:x86 --configuration:Debug
 bin\JSILc "Examples\SimpleRaytracer.sln"
