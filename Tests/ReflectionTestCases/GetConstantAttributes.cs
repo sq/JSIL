bb62ccbc7f3fef15f02c490d7248fb11eebd7aa8
Kevin Gadd <kevin.gadd@gmail.com>
2012-11-21 18:40:52 -0800

    Add support for attaching attributes to constants. Represent constants as special fields.

---
+++ Tests/ReflectionTestCases/GetConstantAttributes.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Reflection;
+
+public static class Program {
+    public static void Main (string[] args) {
+        Common.Util.ListAttributes(typeof(T).GetField("ConstantA"), false);
+        Common.Util.ListAttributes(typeof(T).GetField("ConstantB"), false);
+    }
+}
+
+public class T {
+    [Common.AttributeA]
+    public const int ConstantA = 1;
+    [Common.AttributeA, Common.AttributeB(1, "a")]
+    public const int ConstantB = 2;
+}
