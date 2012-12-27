a931f52d14f3486e0eaef911a6dac1bb381086f7
Markus Johnsson <markus.johnsson@infviz.com>
2012-12-27 11:22:25 +0100

    Added tests for null coalescing of nullables.

--- Tests/SimpleTestCases/NullCoalescing.cs
+++ Tests/SimpleTestCases/NullCoalescing.cs
@@ -13,6 +13,19 @@ public class CustomType {
     }
 }
 
+public struct CustomStruct
+{
+    public readonly int Value;
+
+    public CustomStruct(int value) {
+        Value = value;
+    }
+
+    public override string ToString() {
+        return Value.ToString();
+    }
+}
+
 public static class Program {
     public static void Main (string[] args) {
         var a = new CustomType(1);
@@ -23,5 +36,14 @@ public static class Program {
         Console.WriteLine(b ?? a);
         Console.WriteLine(a ?? c);
         Console.WriteLine(b ?? c);
+
+        CustomStruct? a1 = new CustomStruct(1);
+        CustomStruct? b1 = new CustomStruct(2);
+        CustomStruct? c1 = null;
+
+        Console.WriteLine(a1 ?? b1);
+        Console.WriteLine(b1 ?? a1);
+        Console.WriteLine(a1 ?? c1);
+        Console.WriteLine(b1 ?? c1);
     }
 }
