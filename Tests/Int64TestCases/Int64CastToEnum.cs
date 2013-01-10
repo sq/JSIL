ae7ea53771093210e84be5eb36804142e8fc15f3
Markus Johnsson <markus.johnsson@infviz.com>
2013-01-10 21:41:56 +0100

    Fix issue with casts from long/ulong to enum.

---
+++ Tests/Int64TestCases/Int64CastToEnum.cs
@@ -0,0 +1,15 @@
+﻿
+using System;
+enum E { a, b }
+
+public static class Program
+{
+    public static ulong GetULong() { return 0UL; }
+    public static long GetLong() { return 0L; }
+
+    public static void Main(string[] args)
+    {
+        Console.WriteLine((E)GetULong());
+        Console.WriteLine((E)GetLong());
+    }
+}
