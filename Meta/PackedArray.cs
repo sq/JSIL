6eac000a3a14b2ce7e9e6abc176b1832958294fd
Kevin Gadd <kevin.gadd@gmail.com>
2013-03-18 03:26:14 -0700

    Add a test for another packed array use case Set metadata on packed array interface methods so calls to them can be optimized better

--- Meta/PackedArray.cs
+++ Meta/PackedArray.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using JSIL.Meta;
 
 namespace JSIL.Runtime {
     internal class LinkedTypeAttribute : Attribute {
@@ -13,8 +14,11 @@ namespace JSIL.Runtime {
     }
 
     public unsafe interface IPackedArray<T> {
+        [JSResultIsNew]
         T Get (int index);
         void* GetReference (int index);
+        [JSEscapingArguments("value")]
+        [JSMutatedArguments()]
         void Set (int index, T value);
         int Length { get; }
     }
---
+++ Tests/UnsafeTestCases/FillPackedStructArray.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using JSIL.Meta;
+
+public static class Program {
+    [JSPackedArray]
+    public static IntFloatPair[] PackedArray = new IntFloatPair[8];
+
+    public static unsafe void Main (string[] args) {
+        for (int i = 0, l = PackedArray.Length; i < l; i++) {
+            PackedArray[i].Int = i;
+            PackedArray[i].Float = (i * 1.1f);
+        }
+
+        foreach (var item in PackedArray)
+            Console.WriteLine(item);
+    }
+}
