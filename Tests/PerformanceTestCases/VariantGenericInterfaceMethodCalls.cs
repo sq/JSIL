5a49c38219b8a77b426115ebb0c97e7e9512b8d6
Kevin Gadd <kevin.gadd@gmail.com>
2013-05-13 00:48:41 -0700

    Add a simple benchmark for measuring the cost of variant generic interface method calls.

---
+++ Tests/PerformanceTestCases/VariantGenericInterfaceMethodCalls.cs
@@ -0,0 +1,68 @@
+﻿//@compileroption /unsafe
+
+using System;
+using JSIL.Meta;
+
+public static class Program {
+    const int InnerIterationCount = 4096;
+    const int IterationCount = 256;
+
+    public static unsafe void Main () {
+        Console.WriteLine("Generic Interface: {0:00000.00}ms", Time(TestGenericInterface));
+        Console.WriteLine("Variant Generic Interface: {0:00000.00}ms", Time(TestVariantGenericInterface));
+    }
+
+    public static int Time (Func<string> func) {
+        var started = Environment.TickCount;
+
+        string result = null;
+
+        for (int i = 0; i < IterationCount; i++)
+            result = func();
+
+        Console.WriteLine("{0}", result);
+
+        var ended = Environment.TickCount;
+        return ended - started;
+    }
+
+    public static string TestGenericInterface () {
+        string result = null;
+        IDumbWorker<string> obj = new DumbWorkerObject();
+
+        for (int i = 0; i < InnerIterationCount; i++)
+            result = obj.DoWork();
+
+        return result;
+    }
+
+    public static string TestVariantGenericInterface () {
+        object result = null;
+        var obj = (IDumbWorker<object>)new DumbWorkerObject();
+
+        for (int i = 0; i < InnerIterationCount; i++)
+            result = obj.DoWork();
+
+        return (string)result;
+    }
+}
+
+public interface IDumbWorker<out T> {
+    T DoWork ();
+}
+
+public class DumbWorkerObject : IDumbWorker<string> {
+    const int CharCount = 128;
+
+    public readonly string Prefix;
+    public readonly string Suffix;
+
+    public DumbWorkerObject () {
+        Prefix = new string('a', CharCount);
+        Suffix = new string('b', CharCount);
+    }
+
+    public string DoWork () {
+        return Prefix + Suffix;
+    }
+}
--- Tests/PerformanceTests.cs
+++ Tests/PerformanceTests.cs
@@ -120,6 +120,18 @@ namespace JSIL.Tests {
                 Console.WriteLine("JS:\r\n{0}", test.RunJavascript(null));
             }
         }
+
+        [Test]
+        public void VariantGenericInterfaceMethodCalls () {
+            using (var test = MakeTest(
+                @"PerformanceTestCases\VariantGenericInterfaceMethodCalls.cs"
+            )) {
+                long elapsedcs;
+
+                Console.WriteLine("C#:\r\n{0}", test.RunCSharp(null, out elapsedcs));
+                Console.WriteLine("JS:\r\n{0}", test.RunJavascript(null));
+            }
+        }
     }
 
     [TestFixture]
