02de4cad73402a6a263f9053536cfee27e6b0e7c
Kevin Gadd <kevin.gadd@gmail.com>
2013-05-29 22:34:35 -0700

    Checkpoint: Add tests covering enumeration of class constructors and class methods

---
+++ Tests/ReflectionTestCases/ListClassConstructors.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Reflection;
+
+public static class Program {
+    public static void Main (string[] args) {
+        Common.Util.ListConstructors(
+            typeof(T),
+            BindingFlags.DeclaredOnly | BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic
+        );
+        Common.Util.ListConstructors(
+            typeof(T),
+            BindingFlags.DeclaredOnly | BindingFlags.Instance | BindingFlags.Public 
+        );
+        Common.Util.ListConstructors(
+            typeof(T),
+            BindingFlags.DeclaredOnly | BindingFlags.Instance | BindingFlags.NonPublic
+        );
+    }
+}
+
+public class T {
+    public T () {
+        Console.WriteLine("T()");
+    }
+
+    public T (int i) {
+        Console.WriteLine("T(int)");
+    }
+
+    public T (string s) {
+        Console.WriteLine("T(string)");
+    }
+}
---
+++ Tests/ReflectionTestCases/ListClassMethods.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Reflection;
+
+public static class Program {
+    public static void Main (string[] args) {
+        Console.WriteLine("No public modifier");
+        Common.Util.ListMethods(
+            typeof(T),
+            BindingFlags.Instance | BindingFlags.DeclaredOnly
+        );
+
+        Console.WriteLine("NonPublic");
+        Common.Util.ListMethods(
+            typeof(T),
+            BindingFlags.Instance | BindingFlags.DeclaredOnly | BindingFlags.NonPublic
+        );
+
+        Console.WriteLine("Public");
+        Common.Util.ListMethods(
+            typeof(T),
+            BindingFlags.Instance | BindingFlags.DeclaredOnly | BindingFlags.Public
+        );
+
+        Console.WriteLine("NonPublic | Public");
+        Common.Util.ListMethods(
+            typeof(T),
+            BindingFlags.Instance | BindingFlags.DeclaredOnly | BindingFlags.NonPublic | BindingFlags.Public
+        );
+    }
+}
+
+public class T {
+    private void A () {
+        Console.WriteLine("A()");
+    }
+
+    protected void A (object o) {
+        Console.WriteLine("A(object)");
+    }
+
+    public void A (int i) {
+        Console.WriteLine("A(int)");
+    }
+
+    public void B (string s) {
+        Console.WriteLine("B(string)");
+    }
+}
