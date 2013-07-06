097a9363d031bdfce9d1a0397d8e87547d4f5d7c
Denis Lebedenko <denisv@post.skynet.lt>
2013-07-06 16:51:40 +0300

    Interfaces should have no base type

---
+++ Tests/SimpleTestCases/InterfaceBaseType.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+interface I {
+}
+
+class Program {
+    public static void Main () {
+        Console.WriteLine(typeof(I).BaseType);
+        Console.WriteLine(typeof(I).BaseType == null ? "true" : "false");
+  }
+}
