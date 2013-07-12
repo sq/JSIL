2182d1ca42b8427e1f32b7f9d1567ed09b71df65
K. Gadd <kg@luminance.org>
2013-07-11 21:11:54 -0700

    Site updates

--- jsil.org/index.js
+++ jsil.org/index.js
@@ -64,7 +64,7 @@ function displayBranches (activeBranch, result) {
 };
 
 function loadChangelog (branch) {
-  var username = "kevingadd";
+  var username = "sq";
   var reponame = "JSIL";
   
   if (typeof (branch) === "undefined")
