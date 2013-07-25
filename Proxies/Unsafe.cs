00e4c7caa2cbdd0b4305252d619c7ab3c7cd48c6
K. Gadd <kg@luminance.org>
2013-07-25 06:12:21 -0700

    Enable passing packed arrays to GCHandle.Alloc

---
+++ Proxies/Unsafe.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using JSIL.Meta;
+using JSIL.Proxy;
+
+namespace JSIL.Proxies {
+    [JSProxy(
+        typeof(GCHandle),
+        memberPolicy: JSProxyMemberPolicy.ReplaceNone,
+        attributePolicy: JSProxyAttributePolicy.ReplaceDeclared
+    )]
+    public abstract class GCHandleProxy {
+        [JSAllowPackedArrayArguments]
+        public static GCHandle Alloc (object value) {
+            throw new NotImplementedException();
+        }
+
+        [JSAllowPackedArrayArguments]
+        public static GCHandle Alloc (object value, GCHandleType type) {
+            throw new NotImplementedException();
+        }
+    }
+}
