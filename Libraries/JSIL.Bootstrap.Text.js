a70aaee0224509408b938b9f8917e1b7c09c8bea
Denis Lebedenko <denisv@post.skynet.lt>
2013-07-29 14:20:05 +0300

    Implement char.IsLetter, char.IsLetterOrDigit, char.IsSurrogate for non-Unicode characters; fix char.IsWhiteSpace

--- Libraries/JSIL.Bootstrap.Text.js
+++ Libraries/JSIL.Bootstrap.Text.js
@@ -2019,16 +2019,42 @@ JSIL.ImplementExternals("System.Char", function ($) {
     }
   );
 
+  $.Method({Static:true , Public:true }, "IsLetter", 
+    new JSIL.MethodSignature($.Boolean, [$.Char], []), 
+    function IsLetter (c) {
+      // FIXME: Unicode
+      var charCode = c.charCodeAt(0);
+      return (
+        ((charCode >= 65) && (charCode <= 90)) ||
+        ((charCode >= 97) && (charCode <= 122)));
+    }
+  );
+
+  $.Method({Static:true , Public:true }, "IsLetterOrDigit", 
+    new JSIL.MethodSignature($.Boolean, [$.Char], []), 
+    function IsLetterOrDigit (c) {
+      return $jsilcore.System.Char.IsLetter(c) || $jsilcore.System.Char.IsDigit(c);
+    }
+  );
+
+  $.Method({Static:true , Public:true }, "IsSurrogate", 
+    new JSIL.MethodSignature($.Boolean, [$.Char], []), 
+    function IsSurrogate (c) {
+      var charCode = c.charCodeAt(0);
+      return (charCode >= 0xD800) && (charCode <= 0xDFFF);
+    }
+  );
+
   $.Method({Static:true , Public:true }, "IsWhiteSpace", 
     new JSIL.MethodSignature($.Boolean, [$.Char], []), 
     function IsWhiteSpace (c) {
       // FIXME: Unicode
       var charCode = c.charCodeAt(0);
-      return 
+      return (
         ((charCode >= 0x09) && (charCode <= 0x13)) || 
         (charCode === 0x20) ||
         (charCode === 0xA0) || 
-        (charCode === 0x85);
+        (charCode === 0x85));
     }
   );
 });
---
+++ Tests/SimpleTestCases/CharIs.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class Program {
+    static void Categorize (char c) {
+        Console.WriteLine((int) c);
+        
+        if (char.IsControl(c))
+            Console.WriteLine("IsControl");
+        
+        if (char.IsDigit(c))
+            Console.WriteLine("IsDigit");
+        
+        if (char.IsLetter(c))
+            Console.WriteLine("IsLetter");
+        
+        if (char.IsLetterOrDigit(c))
+            Console.WriteLine("IsLetterOrDigit");
+        
+        if( char.IsSurrogate(c))
+            Console.WriteLine("IsSurrogate");
+        
+        if (char.IsWhiteSpace(c))
+            Console.WriteLine("IsWhiteSpace");
+        
+        Console.WriteLine();
+    }
+    
+    public static void Main () {
+        Categorize(' ');
+        Categorize('4');
+        Categorize('J');
+        Categorize('s');
+        Categorize((char) 0x0D);
+        Categorize((char) 0xD801);
+    }
+}
