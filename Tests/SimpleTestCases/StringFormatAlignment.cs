578ec53c768c4b9d32fdee7d49e61ae046628573
K. Gadd <kg@luminance.org>
2013-09-03 04:21:30 -0700

    Implement basic support for alignment in String.Format format strings - {index,alignment}

---
+++ Tests/SimpleTestCases/StringFormatAlignment.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+public static class Program {
+    public static void Main (string[] args) {
+        int i = 1234;
+        float f = 12.34f;
+
+        Console.WriteLine(String.Format("{0,1}", i));
+        Console.WriteLine(String.Format("{0,1}", f));
+
+        Console.WriteLine(String.Format("{0,4}", i));
+        Console.WriteLine(String.Format("{0,4}", f));
+
+        Console.WriteLine(String.Format("{0,6}", i));
+        Console.WriteLine(String.Format("{0,6}", f));
+
+        Console.WriteLine(String.Format("{0,-1}", i));
+        Console.WriteLine(String.Format("{0,-1}", f));
+
+        Console.WriteLine(String.Format("{0,-6}", i));
+        Console.WriteLine(String.Format("{0,-6}", f));
+    }
+}
