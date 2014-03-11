e963d761a74806b50330340fd683998288c2e3e9
ik <igor@kiselev.spb.ru>
2014-03-11 00:54:17 -0700

    Fix #328. Only System.Xml.XmlConvert declared.

--- Libraries/JSIL.XML.js
+++ Libraries/JSIL.XML.js
@@ -419,15 +419,15 @@ JSIL.ImplementExternals("System.Xml.Serialization.XmlSerializationReader", funct
   $.RawMethod(false, "$getNullAttribute", function () {
     var a = this.r.GetAttribute(this.nilID, this.instanceNsID);
     if (a !== null)
-      return Microsoft.Xml.XmlConvert.ToBoolean(a);
+      return System.Xml.XmlConvert.ToBoolean(a);
 
     a = this.r.GetAttribute(this.nilID, this.instanceNs2000ID);
     if (a !== null)
-      return Microsoft.Xml.XmlConvert.ToBoolean(a);
+      return System.Xml.XmlConvert.ToBoolean(a);
 
     a = this.r.GetAttribute(this.nilID, this.instanceNs1999ID);
     if (a !== null)
-      return Microsoft.Xml.XmlConvert.ToBoolean(a);
+      return System.Xml.XmlConvert.ToBoolean(a);
 
     return false;
   });
