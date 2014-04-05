2b8b8863797b3a5d2b50d615df99be0c9735e358
igor@kiselev.spb.ru <igor@kiselev.spb.ru>
2014-04-05 04:02:20 -0700

    Initial async/await support (#105, #108)

---
+++ Tests/SimpleTestCases/AsyncAwait.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        var source1 = new TaskCompletionSource<object>();
+        var source2 = new TaskCompletionSource<object>();
+
+        Console.WriteLine("Main thread");
+        AsyncMethod(source1.Task, source2.Task).ContinueWith((Task pre) => Console.WriteLine("Continuation:" + ((Task<string>)pre).Result));
+        Console.WriteLine("Main: Delay 1 complete");
+        source1.TrySetResult(string.Empty);
+        Console.WriteLine("Main: Delay 2 complete");
+        source2.TrySetResult(string.Empty);
+
+        if (!JSIL.Builtins.IsJavascript)
+        {
+            Thread.Sleep(100);
+        }
+    }
+
+    public static async Task<string> AsyncMethod(Task task1, Task task2)
+    {
+        await task1;
+        Console.WriteLine("After delay 1");
+        await task2;
+        Console.WriteLine("After delay 2");
+        return "AsyncMethod result";
+    }
+}
