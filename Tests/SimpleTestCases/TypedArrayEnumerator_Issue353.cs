c324a2af8430f195bdaaf62484e58d0b035bb24a
ik <igor@kiselev.spb.ru>
2014-04-25 14:30:43 -0700

    Typed array IEnumerable<T>.GetEnumerator fixed (#353).

---
+++ Tests/SimpleTestCases/TypedArrayEnumerator_Issue353.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        var item = TestInnerArrayCast(GetItems(), it => new[] { it });
+        Console.WriteLine(item.GetType().FullName);
+    }
+
+    private static IEnumerable<BaseClass> GetItems()
+    {
+        return new List<DerivedClass>() { new DerivedClass() };
+    }
+
+    public static BaseClass TestInnerArrayCast(IEnumerable<BaseClass> source, Func<BaseClass, IEnumerable<BaseClass>> selector)
+    {
+        var outerEnumerator = source.GetEnumerator();
+        outerEnumerator.MoveNext();
+
+        var inner = selector(outerEnumerator.Current);
+        var innerEnumerator = inner.GetEnumerator();
+        innerEnumerator.MoveNext();
+
+        return innerEnumerator.Current;
+    }
+}
+
+public class BaseClass
+{
+}
+
+public class DerivedClass : BaseClass
+{
+}
