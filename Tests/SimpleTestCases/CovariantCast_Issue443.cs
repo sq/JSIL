fd59a8e78b8ba0a78986ca9fea91bf40cd96c6a4
igor@kiselev.spb.ru <igor@kiselev.spb.ru>
2014-04-25 18:20:58 -0700

    Fixed covariant cast problem (#443). Simplified #353 test, as it is not hides by #443 anymore.

---
+++ Tests/SimpleTestCases/CovariantCast_Issue443.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        var a = (object)new Implementor<object>();
+        try
+        {
+            var b = (IInterface<string>) a;
+            b.Get();
+        }
+        catch (Exception)
+        {
+            Console.WriteLine("Expected exception");
+        }
+    }
+
+    public static IEnumerable<T> GetTypedArray<T>(Func<IEnumerable<T>> func)
+    {
+        return func();
+    }
+}
+
+public interface IInterface<out T>
+{
+    T Get();
+}
+
+public class Implementor<T> : IInterface<T>
+{
+    public T Get()
+    {
+        Console.WriteLine(typeof(T).Name);
+        return default(T);
+    }
+}
