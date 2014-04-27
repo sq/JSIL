e58df5ec922dfccade48f5b02be7ceaa5a3af300
K. Gadd <kg@luminance.org>
2014-04-27 03:04:55 -0700

    Add tests for issues #410, #411, and #425. Fix issue #425.

---
+++ Tests/SimpleTestCases/Issue425.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+public static class Program {
+    public static void Main (string[] args) {
+        Console.WriteLine(Get().Equals(Get()) ? "true" : "false");
+    }
+
+    public static long Get () {
+        return 1;
+    }
+}
