1fa90be2413ed03b3d66c547062aeafd5a701a56
ik <igor@kiselev.spb.ru>
2014-05-20 00:19:43 -0700

    Interface methods reflection call. Fix #484.

--- Libraries/JSIL.Core.js
+++ Libraries/JSIL.Core.js
@@ -9262,8 +9262,12 @@ JSIL.$GetMethodImplementation = function (method, target) {
   if (method._data.signature.genericArgumentValues) {
     if (isStatic) {
        return result.apply(method.DeclaringType.__PublicInterface__, method._data.signature.genericArgumentValues).bind(method.DeclaringType.__PublicInterface__);
+    } else if (result instanceof JSIL.InterfaceMethod) {
+      return function(methodArgs) { return result.Call(this, method._data.signature.genericArgumentValues, methodArgs) };
     }
     return result.apply(target, method._data.signature.genericArgumentValues);
+  } else if (result instanceof JSIL.InterfaceMethod) {
+    return function(methodArgs) { return result.Call(this, methodArgs); };
   }
   return result;
 };
