6d804c631d27da866111f4e6dd0cad8f41ba45bc
ik <igor@kiselev.spb.ru>
2014-05-25 21:38:17 -0700

    Delegate type tests added.
    --HG--
    extra : source : 39b03e75b66b94653b62ca85279e012e27b7a031
    extra : amend_source : 6ff90972582fed4bde5d67f2f2efffd733832deb

---
+++ Tests/SimpleTestCases/DelegateBaseType_Issue449.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine(typeof(MulticastDelegate).IsAssignableFrom(typeof(Action)) ? "true" : "false");
+    }
+}
---
+++ Tests/SimpleTestCases/DelegateInvoke_Issue500.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        var action = (Action)Test;
+        action.Invoke();
+    }
+
+    public static void Test()
+    {
+        Console.WriteLine("Test");
+    }
+}
