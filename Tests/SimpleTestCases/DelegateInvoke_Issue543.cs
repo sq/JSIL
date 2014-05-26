6c101817b87c37bb09a8a564544890ac705a1802
ik <igor@kiselev.spb.ru>
2014-05-25 21:38:17 -0700

    Delegate type tests added.

---
+++ Tests/SimpleTestCases/DelegateInvoke_Issue543.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        var a = new Action(new Action(A));
+        a();
+        a += A;
+        a = new Action(a);
+        a();
+    }
+
+    public static void A()
+    {
+        Console.WriteLine("A");
+    }
+}
---
+++ Tests/SimpleTestCases/DelegateReflection_Issue500.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        var mi = typeof(object).GetMethod("GetHashCode");
+        Console.WriteLine(mi.Name);
+
+        mi = typeof(Action).GetMethod("NotExistedMethod");
+        if (mi != null)
+        {
+            Console.WriteLine("NotExistedMethod");
+        }
+
+        mi = typeof(Action<A>).GetMethod("Invoke");
+            Console.WriteLine(mi.Name);
+            Console.WriteLine(mi.GetParameters()[0].ParameterType.Name);
+
+        mi = typeof (Generic<A>.InnerDelegate).GetMethod("Invoke");
+        Console.WriteLine(mi.ReturnType.Name);
+    }
+}
+
+public class A {}
+
+public static class Generic<T>
+{
+    public delegate T InnerDelegate();
+}
