6713d5e4dc92b065deb707b668f70ee863d701c3
MichaelD! <michael@dragonspark.us>
2014-05-30 13:57:07 -0700

    Fixes #381 and #508
    Fixes #381 and #508
    Adding support for Generic Closures.  Not the best implementation, but
    fixes the issues.  There might be a better more comprehensive
    implementation around closures at a later point.

---
+++ Tests/SimpleTestCases/Issue381.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        var a = GetGenericList<object>();
+        Console.WriteLine(a);
+    }
+
+    public static List<T> GetGenericList<T>()
+    {
+        Func<List<T>> listCreator = () => new List<T>();
+	    var result = listCreator();
+		return result;
+    }
+}
---
+++ Tests/SimpleTestCases/Issue508.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+    }
+
+    private static void FailingMethod<T>()
+    {
+        Func<GenericClassWithTwoConstructors<T>> f = () => new GenericClassWithTwoConstructors<T>();
+    }
+}
+
+public class GenericClassWithTwoConstructors<T>
+{
+    public GenericClassWithTwoConstructors(string str)
+    {}
+
+    public GenericClassWithTwoConstructors()
+    {}
+}
