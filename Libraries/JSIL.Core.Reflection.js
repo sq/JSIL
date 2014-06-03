ddd860c159c73a15616b2fb84a53a2eabd96fff1
igor@kiselev.spb.ru <igor@kiselev.spb.ru>
2014-06-02 22:29:22 -0700

    Removed call of JSIL.ImplementExternals with $jsilcore.MemberInfoExternals for: PropertyInfo FieldInfo

--- Libraries/JSIL.Core.Reflection.js
+++ Libraries/JSIL.Core.Reflection.js
@@ -565,14 +565,6 @@ JSIL.ImplementExternals(
 );
 
 JSIL.ImplementExternals(
-  "System.Reflection.PropertyInfo", $jsilcore.MemberInfoExternals
-);
-
-JSIL.ImplementExternals(
-  "System.Reflection.FieldInfo", $jsilcore.MemberInfoExternals
-);
-
-JSIL.ImplementExternals(
   "System.Reflection.MethodBase", function ($) {
     $.RawMethod(false, "InitResolvedSignature",
       function InitResolvedSignature() {
