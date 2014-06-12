02776a0541d0a5b94363e8c9ad7136c9617fbe83
ik <igor@kiselev.spb.ru>
2014-06-12 12:00:13 -0700

    Added BCL proxies for classes implemented in JSIL.Async.js

---
+++ Proxies/BCL/JSIL.Async.cs
@@ -0,0 +1,78 @@
+﻿using System.Threading.Tasks;
+
+using JSIL.Meta;
+using JSIL.Proxy;
+
+namespace JSIL.Proxies.Bcl
+{
+    [JSProxy("System.Runtime.CompilerServices.AsyncVoidMethodBuilder", JSProxyMemberPolicy.ReplaceNone, JSProxyAttributePolicy.ReplaceDeclared,
+        JSProxyInterfacePolicy.ReplaceNone, false)]
+    [JSStubOnly]
+    public class System_Runtime_CompilerServices_AsyncVoidMethodBuilder
+    {
+    }
+
+    [JSProxy("System.Runtime.CompilerServices.AsyncVoidMethodBuilder`1", JSProxyMemberPolicy.ReplaceNone, JSProxyAttributePolicy.ReplaceDeclared,
+        JSProxyInterfacePolicy.ReplaceNone, false)]
+    [JSStubOnly]
+    public class System_Runtime_CompilerServices_AsyncTaskMethodBuilder
+    {
+    }
+
+    [JSProxy("System.Runtime.CompilerServices.AsyncTaskMethodBuilder`1", JSProxyMemberPolicy.ReplaceNone, JSProxyAttributePolicy.ReplaceDeclared,
+        JSProxyInterfacePolicy.ReplaceNone, false)]
+    [JSStubOnly]
+    public class System_Runtime_CompilerServices_AsyncTaskMethodBuilder_1
+    {
+    }
+
+    [JSProxy(typeof(Task), JSProxyMemberPolicy.ReplaceNone, JSProxyAttributePolicy.ReplaceDeclared,
+        JSProxyInterfacePolicy.ReplaceNone, false)]
+    [JSStubOnly]
+    public class System_Threading_Tasks_Tasks
+    {
+    }
+
+    [JSProxy(typeof(Task<>), JSProxyMemberPolicy.ReplaceNone, JSProxyAttributePolicy.ReplaceDeclared,
+        JSProxyInterfacePolicy.ReplaceNone, false)]
+    [JSStubOnly]
+    public class System_Threading_Tasks_Tasks_1
+    {
+    }
+
+
+    [JSProxy(typeof(TaskCompletionSource<>), JSProxyMemberPolicy.ReplaceNone, JSProxyAttributePolicy.ReplaceDeclared,
+        JSProxyInterfacePolicy.ReplaceNone, false)]
+    [JSStubOnly]
+    public class System_Threading_Tasks_TaskCompletionSource_1
+    {
+    }
+
+    [JSProxy(typeof(TaskFactory), JSProxyMemberPolicy.ReplaceNone, JSProxyAttributePolicy.ReplaceDeclared,
+        JSProxyInterfacePolicy.ReplaceNone, false)]
+    [JSStubOnly]
+    public class System_Threading_Tasks_TaskFactory
+    {
+    }
+
+    [JSProxy(typeof(TaskFactory<>), JSProxyMemberPolicy.ReplaceNone, JSProxyAttributePolicy.ReplaceDeclared,
+        JSProxyInterfacePolicy.ReplaceNone, false)]
+    [JSStubOnly]
+    public class System_Threading_Tasks_TaskFactory_1
+    {
+    }
+
+    [JSProxy("System.Runtime.CompilerServices.TaskAwaiter", JSProxyMemberPolicy.ReplaceNone, JSProxyAttributePolicy.ReplaceDeclared,
+        JSProxyInterfacePolicy.ReplaceNone, false)]
+    [JSStubOnly]
+    public class System_Runtime_CompilerServices_TaskAwaiter
+    {
+    }
+
+    [JSProxy("System.Runtime.CompilerServices.TaskAwaiter`1", JSProxyMemberPolicy.ReplaceNone, JSProxyAttributePolicy.ReplaceDeclared,
+        JSProxyInterfacePolicy.ReplaceNone, false)]
+    [JSStubOnly]
+    public class System_Runtime_CompilerServices_TaskAwaiter_1
+    {
+    }
+}
