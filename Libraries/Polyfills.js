d03cc9241693a894914db605d950420227d92a4c
K. Gadd <kg@luminance.org>
2014-06-26 02:09:44 -0700

    Introduce Math.imul polyfill

--- Libraries/Polyfills.js
+++ Libraries/Polyfills.js
@@ -49,3 +49,25 @@ if (typeof (Math.fround) === "function") {
     return +d;
   };
 }
+
+if (typeof (Math.imul) === "function") {
+} else { 
+  (function () {
+    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/imul
+
+    Math.imul = function (a, b) {
+      var ah  = (a >>> 16) & 0xffff;
+      var al = a & 0xffff;
+      var bh  = (b >>> 16) & 0xffff;
+      var bl = b & 0xffff;
+      // the shift by 0 fixes the sign on the high part
+      // the final |0 converts the unsigned value into a signed value
+      return (
+        (al * bl) + (
+          ((ah * bl + al * bh) << 16) 
+          >>> 0
+        ) | 0
+      );
+    }
+  })();
+}
