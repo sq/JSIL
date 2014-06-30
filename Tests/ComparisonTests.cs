d53b75cc3e4ae376fe3589e0f790080457c0949a
K. Gadd <kg@luminance.org>
2014-06-29 21:48:47 -0700

    Split SimpleTestCases out from the rest of the test cases so test runners can run them in parallel with the other test groups

--- Tests/ComparisonTests.cs
+++ Tests/ComparisonTests.cs
@@ -513,64 +513,6 @@ namespace JSIL.Tests {
         #region Folders
 
         [Test]
-        [TestCaseSource("SimpleTestCasesSource")]
-        public void SimpleTestCases (object[] parameters) {
-            RunSingleComparisonTestCase(parameters);
-        }
-
-        protected IEnumerable<TestCaseData> SimpleTestCasesSource()
-        {
-            return FolderTestSource("SimpleTestCases", MakeDefaultProvider(), new AssemblyCache());
-        }
-
-        [Test]
-        [TestCaseSource("SimpleTestCasesSourceForTranslatedBcl")]
-        public void SimpleTestCasesForTranslatedBcl(object[] parameters)
-        {
-            Func<Configuration> makeConfiguration = () =>
-                {
-                    var c = new Configuration
-                        {
-                            ApplyDefaults = false,
-                        };
-                    c.Assemblies.Stubbed.Add("^System,");
-                    c.Assemblies.Stubbed.Add("^System\\.(?!Core)(.+),");
-                    c.Assemblies.Stubbed.Add("^Microsoft\\.(.+),");
-                    c.Assemblies.Stubbed.Add("FSharp.Core,");
-
-                    c.Assemblies.Ignored.Add("Microsoft\\.VisualC,");
-                    c.Assemblies.Ignored.Add("Accessibility,");
-                    c.Assemblies.Ignored.Add("SMDiagnostics,");
-                    c.Assemblies.Ignored.Add("System\\.EnterpriseServices,");
-                    c.Assemblies.Ignored.Add("System\\.Security,");
-                    c.Assemblies.Ignored.Add("System\\.Runtime\\.Serialization\\.Formatters\\.Soap,");
-                    c.Assemblies.Ignored.Add("System\\.Runtime\\.DurableInstancing,");
-                    c.Assemblies.Ignored.Add("System\\.Data\\.SqlXml,");
-                    c.Assemblies.Ignored.Add("JSIL\\.Meta,");
-
-                    c.Assemblies.Proxies.Add("JSIL.Proxies.Bcl.dll");
-                    return c;
-                };
-
-            Action<AssemblyTranslator> initializeTranslator = (at) => {
-                // Suppress stdout spew
-                at.IgnoredMethod += (_, _2) => { };
-            };
-
-            RunSingleComparisonTestCase(
-                parameters, 
-                makeConfiguration,
-                false,
-                initializeTranslator: initializeTranslator
-            );
-        }
-
-        protected IEnumerable<TestCaseData> SimpleTestCasesSourceForTranslatedBcl()
-        {
-            return FolderTestSource("SimpleTestCasesForTranslatedBcl", null, new AssemblyCache());
-        }
-
-        [Test]
         [TestCaseSource("JSTestCasesSource")]
         public void JSTestCases (object[] parameters) {
             RunSingleComparisonTestCase(parameters);
