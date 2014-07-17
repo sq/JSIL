4ba3f4d5f4cac806c2504cc4e355dab2fa32b8f1
ik <igor@kiselev.spb.ru>
2014-07-17 15:52:50 -0700

    Fixed incorrect GenericParameter reference in JSIL.CloneParameter (#579).
    --HG--
    extra : amend_source : 50c13029638e81f4f4553f640951c0e26bd58ab7

---
+++ Tests/SimpleTestCases/Issue579.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        var signal1 = new ManualResetEventSlim(false);
+        var signal2 = new ManualResetEventSlim(false);
+
+        var t1 = new TaskCompletionSource<object>();
+        var t2 = new TaskCompletionSource<object>();
+
+        DoWork(t1.Task, 1).ContinueWith(
+            pre =>
+            {
+                if (!JSIL.Builtins.IsJavascript)
+                    Console.Out.Flush();
+
+                signal1.Set();
+            });
+
+        DoWork(t2.Task, 2).ContinueWith(
+            pre =>
+            {
+                if (!JSIL.Builtins.IsJavascript)
+                    Console.Out.Flush();
+
+                signal2.Set();
+            });
+
+        t1.TrySetResult(new object());
+        t2.TrySetResult(new object());
+
+        signal1.Wait();
+        signal2.Wait();
+    }
+
+    public static async Task DoWork(Task waitFor, int index)
+    {
+        Console.WriteLine("Started: " + index);
+        await Program.RunTaskWithTwoInterceptors(
+            "input",
+            async () =>
+                {
+                    Console.WriteLine("Inner begin");
+                    await waitFor;
+                });
+
+        Console.WriteLine("Finished: " + index);
+    }
+
+    public static async Task TaskInterceptor(object target, Func<Task> func)
+    {
+        Console.WriteLine(target);
+        await func();
+    }
+
+    public static Task RunTaskWithTwoInterceptors<TController>(
+        TController target,
+        Func<Task> func)
+    {
+        return TaskInterceptor(
+            target,
+            async () => await TaskInterceptor(target, func));
+    }
+}
