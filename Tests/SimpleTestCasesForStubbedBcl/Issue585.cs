a16db441cd3b8e68d9df8f61f0a76690856c220e
K. Gadd <kg@luminance.org>
2014-07-26 14:45:03 -0700

    Add SimpleTests category for running with stubbed BCL instead of translated BCL.

---
+++ Tests/SimpleTestCasesForStubbedBcl/Issue585.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.ObjectModel;
+
+class Program {
+    public static void Main () {
+        Collection<string> collection = new Collection<string> {
+            "a",
+            "b"
+        };
+
+        foreach (var s in collection) {
+            Console.WriteLine(s);
+        }
+
+        Console.WriteLine(collection.Count);
+
+        var test = collection.GetEnumerator();
+    }
+}
