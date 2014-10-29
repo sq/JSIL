7d80072e8ac170684de6dc63a831502e1bc1c10b
ik <igor@kiselev.spb.ru>
2014-10-29 09:27:38 -0700

    TypedConstExpression added for proper Expression.Constant(object, Type) implemntation.

--- Libraries/JSIL.Bootstrap.Linq.js
+++ Libraries/JSIL.Bootstrap.Linq.js
@@ -686,6 +686,10 @@ JSIL.MakeClass($jsilcore.TypeRef("System.Linq.Expressions.Expression"), "System.
   );
 });
 
+JSIL.MakeClass($jsilcore.TypeRef("System.Linq.Expressions.ConstantExpression"), "System.Linq.Expressions.TypedConstantExpression", true, [], function ($) {
+  var $thisType = $.publicInterface;
+});
+
 JSIL.ImplementExternals("System.Linq.Expressions.ConstantExpression", function ($) {
   $.Method({Static:false, Public:false}, ".ctor", 
     (new JSIL.MethodSignature(null, [$.Object], [])), 
@@ -697,10 +701,30 @@ JSIL.ImplementExternals("System.Linq.Expressions.ConstantExpression", function (
   $.Method({Static:true , Public:false}, "Make", 
     (new JSIL.MethodSignature($jsilcore.TypeRef("System.Linq.Expressions.ConstantExpression"), [$.Object, $jsilcore.TypeRef("System.Type")], [])), 
     function Make (value, type) {
-      return new System.Linq.Expressions.ConstantExpression(value);
+      if (value == null && type == $jsilcore.System.Object.__Type__ || value != null && JSIL.GetType(value) == type) {
+        return new System.Linq.Expressions.ConstantExpression(value);
+      } else {
+        return new System.Linq.Expressions.TypedConstantExpression(value, type);
+      }
+    }
+  );
+});
+
+JSIL.ImplementExternals("System.Linq.Expressions.TypedConstantExpression", function ($) {
+  $.Method({Static:false, Public:false}, ".ctor", 
+    (new JSIL.MethodSignature(null, [$.Object, $jsilcore.TypeRef("System.Type")], [])), 
+    function _ctor (value, type) {
+      this._value = value;
+      this._type = type;
     }
   );
 
+  $.Method({Static:false , Public:true}, "get_Type", 
+    new JSIL.MethodSignature($jsilcore.TypeRef("System.Type"), [], []), 
+    function get_Type() {
+      return this._type;
+    }
+  );
 });
 
 JSIL.MakeClass($jsilcore.TypeRef("System.Linq.Expressions.Expression"), "System.Linq.Expressions.ParameterExpression", true, [], function ($) {
