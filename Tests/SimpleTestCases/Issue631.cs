ea7be7f1761b4cab45f6e806fb61f54d146a2cba
K. Gadd <kg@luminance.org>
2014-12-05 01:06:34 -0800

    Add issue #631 test case

---
+++ Tests/SimpleTestCases/Issue631.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+public static class Program {
+    public static void Main (string[] args) {
+        Dictionary<string, int> dict = new Dictionary<string, int>();
+        dict["one"] = 1;
+        dict["two"] = 2;
+        dict["three"] = 3;
+
+        foreach (KeyValuePair<string, int> pair in dict) {
+            Console.WriteLine("Key: " + pair.Key + ", Value: " + pair.Value);
+        }
+    }
+}
