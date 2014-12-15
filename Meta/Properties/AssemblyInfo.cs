d7832f57b52722a6662233ea2ecdbc41471a488d
Daniel Mirmilshteyn <dmirmilshteyn@users.noreply.github.com>
2014-12-15 18:27:00 -0500

    Moved JSIL.Meta to a PCL

--- Meta/Properties/AssemblyInfo.cs
+++ Meta/Properties/AssemblyInfo.cs
@@ -14,14 +14,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("c09dc0b4-91bf-4772-857a-8d6a885ba71d")]
-
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
