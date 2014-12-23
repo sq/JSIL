5a9c12a358ab7b8e81d8b5f52daf1540e4d05b99
Jaiden Mispy <jaiden@mispy.me>
2014-12-23 19:04:56 +1100

    Don't treat bools as integers for casting (#478)
    Giving them their own castSpecialType means they will use the default
    castFunction instead, which raises an InvalidCastException.

---
+++ Tests/SimpleTestCases/Issue478.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+public static class Program {
+    public static void Main(string[] args) {
+        object obj = "string";
+        obj = Hello.One;
+        Console.WriteLine((int)obj); //Output: 52
+        Console.WriteLine((System.Int32)obj); //Output : 52
+
+        try {
+            Console.WriteLine((float)obj);
+        } catch (System.InvalidCastException) {
+            Console.WriteLine("invalid cast exception (float)");
+        }
+
+        try {
+            Console.WriteLine((bool)obj);
+        }
+        catch (System.InvalidCastException) {
+            Console.WriteLine("invalid cast exception (bool)");
+        }
+
+        try {
+            Console.WriteLine((string)obj);
+        }
+        catch (System.InvalidCastException) {
+            Console.WriteLine("invalid cast exception (string)");
+        }
+
+        try {
+            Console.WriteLine((System.Int64)obj);
+        }
+        catch (System.InvalidCastException) {
+            Console.WriteLine("invalid cast exception (System.Int64)");
+        }
+    }
+
+    public enum Hello {
+        One = 52,
+        Two
+    }
+}
