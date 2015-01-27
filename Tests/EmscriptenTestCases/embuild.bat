b2fd8b8d10663b54828f4af01d769fb633b225db
K. Gadd <kg@luminance.org>
2015-01-27 06:14:05 -0800

    Emscripten test fixture automatically compiles DLL and emscripten JS

--- Tests/EmscriptenTestCases/embuild.bat
+++ Tests/EmscriptenTestCases/embuild.bat
@@ -1,4 +1,28 @@
 @del /Q *.emjs
-@call emcc -Os -O2 --closure 1 --memory-init-file 0 -o common.js common/common.cpp
+@del /Q *.js.map
+
+@if exist "%ProgramW6432%\Emscripten\emsdk_env.bat" (
+    @call "%ProgramW6432%\Emscripten\emsdk_env.bat" 
+) else (
+    @call "%PROGRAMFILES%\Emscripten\emsdk_env.bat"
+)
+@if errorlevel 1 goto fail
+
+@rem @call emcc -O0 -g4 --memory-init-file 0 -o common.js common/common.cpp
+@if errorlevel 1 goto fail
+
+@call emcc -Os -O2 --memory-init-file 0 -o common.js common/common.cpp
+@if errorlevel 1 goto fail
+
+@echo Compiled common.emjs
+
 @rename common.js common.emjs
 @del /Q *.mem
+
+@goto batch_end
+
+:fail
+@echo FAILED.
+@exit /b 1234
+
+:batch_end
