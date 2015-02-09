aa3717af89acedc6b707a2d79a5d10b013027c79
Jaiden Mispy <jaiden@mispy.me>
2015-02-09 21:48:01 +1100

    CustomStringMarshaler test case
    Comes from SDL2-CS, which returns all native strings using LPUtf8StrMarshaler

---
+++ Tests/EmscriptenTestCases/CustomStringMarshaler.cs
@@ -0,0 +1,75 @@
+using System;
+using System.Text;
+using System.Runtime.InteropServices;
+
+public static class Program {
+    internal unsafe class LPUtf8StrMarshaler : ICustomMarshaler {
+        public const string LeaveAllocated = "LeaveAllocated";
+
+        private static ICustomMarshaler
+            _leaveAllocatedInstance = new LPUtf8StrMarshaler(true),
+            _defaultInstance = new LPUtf8StrMarshaler(false);
+
+        public static ICustomMarshaler GetInstance(string cookie) {
+            switch (cookie) {
+                case "LeaveAllocated":
+                    return _leaveAllocatedInstance;
+                default:
+                    return _defaultInstance;
+            }
+        }
+
+        private bool _leaveAllocated;
+
+        public LPUtf8StrMarshaler(bool leaveAllocated) {
+            _leaveAllocated = leaveAllocated;
+        }
+
+        public object MarshalNativeToManaged(IntPtr pNativeData) {
+            if (pNativeData == IntPtr.Zero)
+                return null;
+            var ptr = (byte*)pNativeData;
+            while (*ptr != 0) {
+                ptr++;
+            }
+            var bytes = new byte[ptr - (byte*)pNativeData];
+            Marshal.Copy(pNativeData, bytes, 0, bytes.Length);
+            return Encoding.UTF8.GetString(bytes);
+        }
+
+        public IntPtr MarshalManagedToNative(object ManagedObj) {
+            if (ManagedObj == null)
+                return IntPtr.Zero;
+            var str = ManagedObj as string;
+            if (str == null) {
+                throw new ArgumentException("ManagedObj must be a string.", "ManagedObj");
+            }
+            var bytes = Encoding.UTF8.GetBytes(str);
+            var mem = Marshal.AllocHGlobal(bytes.Length + 1);
+            Marshal.Copy(bytes, 0, mem, bytes.Length);
+            ((byte*)mem)[bytes.Length] = 0;
+            return mem;
+        }
+
+        public void CleanUpManagedData(object ManagedObj) {
+        }
+
+        public void CleanUpNativeData(IntPtr pNativeData) {
+            if (!_leaveAllocated) {
+                Marshal.FreeHGlobal(pNativeData);
+            }
+        }
+
+        public int GetNativeDataSize() {
+            return -1;
+        }
+    }
+
+    [DllImport("common.dll", CallingConvention=CallingConvention.Cdecl)]
+    [return: MarshalAs(UnmanagedType.CustomMarshaler, MarshalTypeRef = typeof(LPUtf8StrMarshaler), MarshalCookie = LPUtf8StrMarshaler.LeaveAllocated)]
+    public static extern string ReturnString();
+
+    public static void Main () {
+        Console.WriteLine(ReturnString());
+    }
+}
