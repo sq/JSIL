a58c19a684a6c7f59808892b1b447ab617cb8dd1
K. Gadd <kg@luminance.org>
2015-02-13 04:55:08 -0800

    Fix issue #682

---
+++ Proxies/Char.cs
@@ -0,0 +1,41 @@
+﻿#pragma warning disable 0660
+#pragma warning disable 0661
+
+using System;
+using System.Collections.Generic;
+using JSIL.Meta;
+using JSIL.Proxy;
+
+namespace JSIL.Proxies {
+    [JSProxy(
+        typeof(char),
+        JSProxyMemberPolicy.ReplaceDeclared
+    )]
+    public abstract class CharProxy {
+        [JSReplacement("$c.toLowerCase()")]
+        [JSIsPure]
+        public static char ToLower (char c) {
+            throw new InvalidOperationException();
+        }
+
+        // FIXME: Are ECMAScript strings always normalized?
+        [JSReplacement("$c.toLowerCase()")]
+        [JSIsPure]
+        public static char ToLowerInvariant (char c) {
+            throw new InvalidOperationException();
+        }
+
+        [JSReplacement("$c.toUpperCase()")]
+        [JSIsPure]
+        public static char ToUpper (char c) {
+            throw new InvalidOperationException();
+        }
+
+        // FIXME: Are ECMAScript strings always normalized?
+        [JSReplacement("$c.toUpperCase()")]
+        [JSIsPure]
+        public static char ToUpperInvariant (char c) {
+            throw new InvalidOperationException();
+        }
+    }
+}
---
+++ Tests/SimpleTestCases/Issue682.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+public static class Program {
+    public static void Main (string[] args) {
+        Console.WriteLine(char.ToLower('a'));
+        Console.WriteLine(char.ToUpper('a'));
+        Console.WriteLine(char.ToLower('A'));
+        Console.WriteLine(char.ToUpper('A'));
+    }
+}
