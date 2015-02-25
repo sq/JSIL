756d3aad98cdc8170dda3a19a02d924d971a6397
Jaiden Mispy <jaiden@mispy.me>
2015-02-25 13:10:59 +1100

    Attempting to make test for proxy extern issue

---
+++ Tests/EmscriptenTestCases/ProxiedExternFunction.cs
@@ -0,0 +1,100 @@
+using System;
+using System.Text;
+using System.Runtime.InteropServices;
+using JSIL;
+using JSIL.Proxy;
+
+internal unsafe class LPUtf8StrMarshaler : ICustomMarshaler {
+    public const string LeaveAllocated = "LeaveAllocated";
+
+    private static ICustomMarshaler
+        _leaveAllocatedInstance = new LPUtf8StrMarshaler(true),
+        _defaultInstance = new LPUtf8StrMarshaler(false);
+
+    public static ICustomMarshaler GetInstance(string cookie) {
+        switch (cookie) {
+            case "LeaveAllocated":
+                return _leaveAllocatedInstance;
+            default:
+                return _defaultInstance;
+        }
+    }
+
+    private bool _leaveAllocated;
+
+    public LPUtf8StrMarshaler(bool leaveAllocated) {
+        _leaveAllocated = leaveAllocated;
+    }
+
+    public object MarshalNativeToManaged(IntPtr pNativeData) {
+        Console.WriteLine("NativeToManaged");
+        if (pNativeData == IntPtr.Zero)
+            return null;
+        var ptr = (byte*)pNativeData;
+        while (*ptr != 0) {
+            ptr++;
+        }
+        var bytes = new byte[ptr - (byte*)pNativeData];
+        Marshal.Copy(pNativeData, bytes, 0, bytes.Length);
+        return Encoding.UTF8.GetString(bytes);
+    }
+
+    public IntPtr MarshalManagedToNative(object ManagedObj) {
+        Console.WriteLine("ManagedToNative");
+        if (ManagedObj == null)
+            return IntPtr.Zero;
+        var str = ManagedObj as string;
+        if (str == null) {
+            throw new ArgumentException("ManagedObj must be a string.", "ManagedObj");
+        }
+        var bytes = Encoding.UTF8.GetBytes(str);
+        var mem = Marshal.AllocHGlobal(bytes.Length + 1);
+        Marshal.Copy(bytes, 0, mem, bytes.Length);
+        ((byte*)mem)[bytes.Length] = 0;
+        return mem;
+    }
+
+    public void CleanUpManagedData(object ManagedObj) {
+    }
+
+    public void CleanUpNativeData(IntPtr pNativeData) {
+        if (!_leaveAllocated) {
+            Marshal.FreeHGlobal(pNativeData);
+        }
+    }
+
+    public int GetNativeDataSize() {
+        return -1;
+    }
+}
+
+public class TestClass {
+//    [DllImport("common.dll", CallingConvention = CallingConvention.Cdecl)]
+//    [return: MarshalAs(UnmanagedType.CustomMarshaler, MarshalTypeRef = typeof(LPUtf8StrMarshaler), MarshalCookie = LPUtf8StrMarshaler.LeaveAllocated)]
+    public string ReturnString(
+//        [In()] [MarshalAs(UnmanagedType.CustomMarshaler, MarshalTypeRef = typeof(LPUtf8StrMarshaler))]
+		string s
+    ) {
+        return s;
+    }
+}
+
+[JSProxy(
+    typeof(TestClass),
+    JSProxyMemberPolicy.ReplaceDeclared
+)]
+public abstract class TestClassProxy {
+    public string ReturnString(string s) {
+        if (Builtins.IsJavascript) {
+            Console.WriteLine("oh no!");
+        }
+        return "Windows";
+    }
+}
+
+public static class Program {
+    public static void Main() {
+        var tc = new TestClass();
+        Console.WriteLine(tc.ReturnString("cheeks"));
+    }
+}
