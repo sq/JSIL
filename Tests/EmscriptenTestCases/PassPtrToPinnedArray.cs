c0ab789bfee2d0d94fb3c2e1771ee6c82005e4ea
Jaiden Mispy <jaiden@mispy.me>
2015-03-12 01:17:50 +1100

    Add test for passing GCHandle ptr to emscripten

---
+++ Tests/EmscriptenTestCases/PassPtrToPinnedArray.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Runtime.InteropServices;
+
+public static unsafe class Program {
+    [DllImport("common.dll", CallingConvention = CallingConvention.Cdecl)]
+    public static extern ushort FirstElementOfUshortArray(IntPtr ptr);
+
+    public static void Main () {
+        var arr = new ushort[5] { 1, 2, 3, 4, 5 };        
+        GCHandle dataHandle = GCHandle.Alloc(arr, GCHandleType.Pinned);
+        Console.WriteLine("Test: {0}", FirstElementOfUshortArray((IntPtr) dataHandle.AddrOfPinnedObject().ToInt64()));
+    }
+}
