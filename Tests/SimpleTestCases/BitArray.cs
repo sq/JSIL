1f1129bc165e2e90264a13763291f35ff7cb75f8
Jaiden Mispy <jaiden@mispy.me>
2015-03-13 05:09:18 +1100

    Test for System.Collections.BitArray
    FNA needs this as part of the content reading process

---
+++ Tests/SimpleTestCases/BitArray.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+
+public static class Program { 
+    public static void Main (string[] args) {
+        var b = new BitArray(5);
+        b[0] = true;
+        b[1] = false;
+
+        for (var i = 0; i < b.Length; i += 1) {
+            Console.WriteLine("{0}: {1}", i, b[i]);
+        }
+    }
+}
