cbc79194f6ef9047bfc67c4c0f6161baebbef3c4
K. Gadd <kg@luminance.org>
2015-06-20 03:15:26 -0700

    Custom padding using FieldOffset works

--- Tests/UnsafeTestCases/CustomFieldOffset.cs
+++ Tests/UnsafeTestCases/CustomFieldOffset.cs
@@ -3,23 +3,20 @@ using System.Runtime.InteropServices;
 
 public static class Program {
     [StructLayout(LayoutKind.Explicit)]
-    public struct Test {
+    public struct Padded {
         [FieldOffset(0)]
         public byte A;
 
-        [FieldOffset(1)]
-        public byte B;
+        [FieldOffset(2)]
+        public uint B;
 
-        [FieldOffset(0)]
-        public uint C;
-
-        [FieldOffset(6)]
-        public float D;
+        [FieldOffset(9)]
+        public float C;
 
         public override string ToString () {
             return String.Format(
-                "A={0:X2}, B={1:X2}, C={2:X8}, D={3:00.000}",
-                A, B, C, D
+                "A={0:X2}, B={1:X8}, C={2:00.000}",
+                A, B, C
             );
         }
     }
@@ -29,9 +26,9 @@ public static class Program {
 
         fixed (byte *pBuffer = buffer) {
             Array.Clear(buffer, 0, 32);
-            *((Test*)pBuffer) = new Test { C = 0xFAEBDCCD, D = 3.3f };
+            *((Padded*)pBuffer) = new Padded { A = 0x1B, B = 0xFAEBDCCD, C = 3.3f };
             Util.PrintBytes(buffer);
-            Console.WriteLine(*((Test*)pBuffer));
+            Console.WriteLine(*((Padded*)pBuffer));
         }
     }
 }
