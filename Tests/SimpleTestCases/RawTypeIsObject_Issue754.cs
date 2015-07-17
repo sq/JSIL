197be154bf2ec7e6dccfac9b700664d075a121be
ik <igor@kiselev.spb.ru>
2015-07-16 22:23:05 -0700

    Fix raw types is object check (#754).

---
+++ Tests/SimpleTestCases/RawTypeIsObject_Issue754.cs
@@ -0,0 +1,16 @@
+using System;
+
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        IsType<object>("qwerty");
+        IsType<object>(1);
+        IsType<object>(new object());
+    }
+
+    public static void IsType<T>(object obj)
+    {
+        Console.Write(obj is T ? "true" : "false");
+    }
+}
