2b608bbe26d2b49609d0d790eff4501d584398a3
ik <igor@kiselev.spb.ru>
2015-07-29 19:07:16 -0700

    .Net Framework 4.6 compatibility fix (temporary exclude System.Resources.FastResourceComparer from translation)

---
+++ Proxies/Net46.cs
@@ -0,0 +1,11 @@
+﻿namespace JSIL.Proxies
+{
+    using JSIL.Meta;
+    using JSIL.Proxy;
+
+    [JSProxy("System.Resources.FastResourceComparer")]
+    [JSStubOnly]
+    public class System_Resources_FastResourceComparer
+    {
+    }
+}
