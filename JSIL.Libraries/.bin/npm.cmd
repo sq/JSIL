942021cce4ab5b61236961976b7ce3b83668c9a1
ik <igor@kiselev.spb.ru>
2015-08-04 17:06:00 -0700

    Moved *.js files from Libraries to JSIL.Libraries project.

---
+++ JSIL.Libraries/.bin/npm.cmd
@@ -0,0 +1,4 @@
+@echo off
+SET PATH=%~dp0;%PATH%
+SET npm_config_git=%~dp0git.cmd
+"%~dp0node" "%~dp0..\..\packages\Npm.1.4.15.2\node_modules\npm\bin\npm-cli.js" %*
---
+++ JSIL.Libraries/gruntfile.js
@@ -0,0 +1,25 @@
+﻿module.exports = function (grunt) {
+    'use strict';
+
+    grunt.loadNpmTasks('grunt-preprocessor');
+
+    grunt.initConfig({
+        preprocessor: {
+            main: {
+                options:
+                {
+                    root: "Includes"
+                },
+                files: [{
+                    expand: true,
+                    cwd: 'Sources',
+                    src: ['**/*.js'],
+                    dest: '../Libraries'
+                }]
+            }
+        }
+    });
+
+    grunt.registerTask('build-Debug', ['preprocessor']);
+    grunt.registerTask('build-Release', ['preprocessor']);
+};
