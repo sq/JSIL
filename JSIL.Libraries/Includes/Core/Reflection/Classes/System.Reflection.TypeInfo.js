78a8782021208e683d88e1ab5c00fdac6adce71c
Daniel Mirmilshteyn <daniel.mirmilshteyn@outlook.com>
2015-08-15 13:11:15 -0400

    Minimal handwritten implementation of TypeInfo and GetTypeInfo

---
+++ JSIL.Libraries/Includes/Core/Reflection/Classes/System.Reflection.TypeInfo.js
@@ -0,0 +1,30 @@
+﻿JSIL.ImplementExternals(
+  "System.Reflection.TypeInfo", function ($) {
+      $.RawMethod(false, "$internalCtor",
+        function (rawType) {
+            this._type = rawType;
+        }
+      );
+
+      $.Method({ Static: false, Public: true }, "AsType",
+        new JSIL.MethodSignature($jsilcore.TypeRef("System.Type"), [], []),
+        function AsType() {
+            return this._type;
+        }
+      );
+
+      $.Method({ Static: false, Public: true, Virtual: true }, "IsAssignableFrom",
+        new JSIL.MethodSignature($.Boolean, [$.Type]),
+        function IsAssignableFrom(result, testType) {
+            return this._type.IsAssignableFrom(testType);
+        }
+      )
+
+      $.Method({ Static: false, Public: true }, "get_Assembly",
+        new JSIL.MethodSignature($jsilcore.TypeRef("System.Reflection.Assembly"), [], []),
+        function get_Assembly() {
+            return this._type.get_Assembly();
+        }
+      );
+  }
+);
