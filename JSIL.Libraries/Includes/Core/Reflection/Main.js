2c6f7ac9a78f94cd7f33ede888095b8708f28014
Daniel Mirmilshteyn <daniel.mirmilshteyn@outlook.com>
2015-08-22 22:45:03 -0400

    Implemented CustomAttributeExtensions.GetCustomAttribute
    Conflicts:
    	JSIL.Libraries/JSIL.Libraries.csproj

--- JSIL.Libraries/Includes/Core/Reflection/Main.js
+++ JSIL.Libraries/Includes/Core/Reflection/Main.js
@@ -14,6 +14,8 @@ if (!$jsilcore)
 //? include("Classes/System.Reflection.PropertyInfo.js"); writeln();
 //? include("Classes/System.Reflection.FieldInfo.js"); writeln();
 
+//? include("Classes/System.Reflection.CustomAttributeExtensions.js"); writeln();
+
 //? include("Classes/System.Reflection.MethodBase.js"); writeln();
 //? include("Classes/System.Reflection.MethodInfo.js"); writeln();
 
