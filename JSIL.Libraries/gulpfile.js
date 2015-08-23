f367d8d712095ab90469fffd7213442758c1ef4a
ik <igor@kiselev.spb.ru>
2015-08-23 01:04:05 -0700

    Migrated to Gulp with custom msbuild integration.

---
+++ JSIL.Libraries/gulpfile.js
@@ -0,0 +1,37 @@
+﻿var gulp = require('gulp');
+var gutil = require('gulp-util');
+var metascriptPipe = require('gulp-metascript');
+
+gulp.task('metascript', function () {
+  return gulp.src('Sources/**/*.js')
+        .pipe(workDirFix(metascriptPipe()))
+        .on('error', logError)
+        .pipe(gulp.dest('../Libraries/'));
+});
+
+gulp.task('build-Debug', ['metascript']);
+gulp.task('build-Release', ['metascript']);
+
+
+
+/*Helpers*/
+function workDirFix(innerPipe) {
+  var through = require('through2'), path = require('path');
+  function fixWorkingDirectory(file, encoding, callback) {
+    var dir = process.cwd();
+    try {
+      process.chdir(path.dirname(file.path));
+      return innerPipe._transform(file, encoding, callback);
+    } catch (e) {
+      console.log("!!!")
+    } finally {
+      process.chdir(dir);
+    }
+  }
+  return through.obj(fixWorkingDirectory);
+}
+
+function logError(error) {
+  console.error(error.message);
+  process.exit(1);
+}
