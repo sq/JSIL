28b5fe54ced8b9545831456c47b830f3ddf9e0ee
ik <igor@kiselev.spb.ru>
2015-08-24 11:34:23 -0700

    gulp-metascript updated to latest version, custom fix removed.

--- JSIL.Libraries/gulpfile.js
+++ JSIL.Libraries/gulpfile.js
@@ -1,10 +1,9 @@
 ﻿var gulp = require('gulp');
-var gutil = require('gulp-util');
 var metascriptPipe = require('gulp-metascript');
 
 gulp.task('metascript', function () {
   return gulp.src('Sources/**/*.js')
-        .pipe(workDirFix(metascriptPipe()))
+        .pipe(metascriptPipe())
         .on('error', logError)
         .pipe(gulp.dest('../Libraries/'));
 });
@@ -15,22 +14,6 @@ gulp.task('build-Release', ['metascript']);
 
 
 /*Helpers*/
-function workDirFix(innerPipe) {
-  var through = require('through2'), path = require('path');
-  function fixWorkingDirectory(file, encoding, callback) {
-    var dir = process.cwd();
-    try {
-      process.chdir(path.dirname(file.path));
-      return innerPipe._transform(file, encoding, callback);
-    } catch (e) {
-      console.log("!!!")
-    } finally {
-      process.chdir(dir);
-    }
-  }
-  return through.obj(fixWorkingDirectory);
-}
-
 function logError(error) {
   console.error(error.message);
   process.exit(1);
