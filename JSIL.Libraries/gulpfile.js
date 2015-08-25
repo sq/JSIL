7748c209c6e45a68504ef5f1d8be319b3f90b967
ik <igor@kiselev.spb.ru>
2015-08-25 00:23:20 -0700

    Header for generated scripts, generated scripts are readonly (#818).

--- JSIL.Libraries/gulpfile.js
+++ JSIL.Libraries/gulpfile.js
@@ -1,15 +1,25 @@
 ﻿var gulp = require('gulp');
 var metascriptPipe = require('gulp-metascript');
+var headerPipe = require('gulp-header');
+var chmod = require('gulp-chmod');
+var clean = require('gulp-clean');
 
 gulp.task('metascript', function () {
   return gulp.src('Sources/**/*.js')
-        .pipe(metascriptPipe())
-        .on('error', logError)
-        .pipe(gulp.dest('../Libraries/'));
+    .pipe(metascriptPipe())
+    .pipe(headerPipe("/* It is auto-generated file. Do not modify it. */\n"))
+    .pipe(chmod({ write: false }))
+    .on('error', logError)
+    .pipe(gulp.dest('../Libraries/'));
 });
 
-gulp.task('build-Debug', ['metascript']);
-gulp.task('build-Release', ['metascript']);
+gulp.task('clean', function () {
+  return gulp.src('../Libraries/', { read: false })
+    .pipe(clean({ force: true }));
+});
+
+gulp.task('build-Debug', ['clean', 'metascript']);
+gulp.task('build-Release', ['clean', 'metascript']);
 
 
 
