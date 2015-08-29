2bd1fe2453a9176839da0a757d0a223e9d707473
K. Gadd <kg@luminance.org>
2015-08-29 02:20:44 -0700

    Add a test for issue #743

---
+++ Tests/SimpleTestCases/Issue743.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public static class Program {
+    public static void Main (string[] args) {
+        var tt = new TestType();
+        tt[1, 2] = 3.5;
+
+        Console.WriteLine(tt._00);
+    }
+}
+
+public class TestType {
+    public double _00;
+
+    public double this[int row, int col]
+    {
+        set
+        {
+            _00 = value;
+        }
+    }
+}
