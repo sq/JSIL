557aab789021b26ec2478fac8df0c73722401319
Katelyn Gadd <kg@luminance.org>
2015-08-30 03:47:49 -0700

    Dead code cleanup

--- build_demos.bat
+++ build_demos.bat
@@ -4,10 +4,6 @@
 @del /s/q jsil.org\demos\*.js
 @title Building TouchThumbSticks
 bin\JSILc "Examples\ThirdParty\TouchThumbSticks\TouchThumbSticks.sln" "jsil.org\demos\TouchThumbSticks\TouchThumbSticks.jsilconfig" --platform=x86 --configuration=Debug
-@title Building Game Type
-bin\JSILc "C:\Game Design\Game Type\CodeNew\GameType\GameType.sln" "jsil.org\demos\GameType\GameType.jsilconfig" --platform:x86 --configuration:Debug
-@title Building Bytown Lumberjack
-bin\JSILc "C:\Users\Kate\Documents\Projects\lumberjack\LumberjackPC.sln" "C:\Users\Kate\Documents\Projects\lumberjack\Lumberjack\Lumberjack\bin\x86\Debug\Lumberjack.XmlSerializers.dll" "jsil.org\demos\Lumberjack\Lumberjack.jsilconfig" --platform:x86 --configuration:Debug
 @title Building Procedural Textures
 bin\JSILc "Examples\ProceduralTextures\ProceduralTextures.sln"
 @title Building Raytracer
@@ -17,8 +13,6 @@ bin\JSILc "Examples\SimplePathtracer.sln"
 @title Building WebGL
 bin\JSILc "Examples\WebGL\WebGL.sln"
 bin\JSILc "Examples\WebGL_Vertex_Structs\WebGL_Vertex_Structs.sln"
-@title Building Tetris
-bin\JSILc "Examples\ThirdParty\Tetris\tetris.sln" "jsil.org\demos\Tetris\Tetris.jsilconfig" --platform=x86 --configuration=Debug
 @title Building Platformer Starter Kit
 bin\JSILc "C:\Users\Kate\Documents\Projects\PlatformerStarterKit\Platformer (Windows).sln" "jsil.org\demos\Platformer\Platformer.jsilconfig" --platform=x86 --configuration=Debug
 @title Building RPG Starter Kit
--- build_skeletons.bat
+++
@@ -1,1 +0,0 @@
-bin\JSILc "Microsoft.Xna.Framework.Game, Version=4.0.0.0, Culture=neutral, PublicKeyToken=842cf8be1de50553" "Microsoft.Xna.Framework.Xact, Version=4.0.0.0, Culture=neutral, PublicKeyToken=842cf8be1de50553" "Microsoft.Xna.Framework.Storage, Version=4.0.0.0, Culture=neutral, PublicKeyToken=842cf8be1de50553" "Skeletons\skeletons.jsilconfig"
