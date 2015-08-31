89ece1a87c521da0d48bf04712e822e24d524cb8
Katelyn Gadd <kg@luminance.org>
2015-08-31 14:21:36 -0700

    Add scripts to build mono from source and activate it for linux scenarios where packages don't work

---
+++ activate-mono.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+SCRIPTPATH=`realpath "$BASH_SOURCE"`
+JSILDIR=`dirname $SCRIPTPATH`
+MONODIR=$JSILDIR/mono
+
+pushd $MONODIR
+
+MONOSOURCEDIR=`realpath ./mono-*`
+MONO_PREFIX=`realpath $MONOSOURCEDIR/build`
+
+popd
+
+export DYLD_FALLBACK_LIBRARY_PATH=$MONO_PREFIX/lib:$DYLD_LIBRARY_FALLBACK_PATH
+export LD_LIBRARY_PATH=$MONO_PREFIX/lib:$LD_LIBRARY_PATH
+export C_INCLUDE_PATH=$MONO_PREFIX/include:$GNOME_PREFIX/include
+export ACLOCAL_PATH=$MONO_PREFIX/share/aclocal
+export PKG_CONFIG_PATH=$MONO_PREFIX/lib/pkgconfig:$GNOME_PREFIX/lib/pkgconfig
+export PATH=$MONO_PREFIX/bin:$PATH
---
+++ install-mono.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+SCRIPTPATH=`realpath "$BASH_SOURCE"`
+JSILDIR=`dirname $SCRIPTPATH`
+MONODIR=$JSILDIR/mono
+
+mkdir $MONODIR
+pushd $MONODIR
+
+echo // Downloading mono source
+rm *.tar.bz2
+curl -sS "http://download.mono-project.com/sources/mono/mono-4.0.4.1.tar.bz2" -o ./mono.tar.bz2
+
+echo // Unpacking
+tar -xf mono.tar.bz2
+
+MONOSOURCEDIR=`realpath ./mono-*`
+pushd $MONOSOURCEDIR
+
+echo // Configuring
+./configure --prefix=$MONOSOURCEDIR/build
+
+echo // Making
+make
+
+echo // Installing
+make install
+
+popd
+
+echo // Done. Built in $MONOSOURCEDIR/build.
+
+popd
