3bc2b64bd520edaa66d3e2587c7b393879269214
Katelyn Gadd <kg@luminance.org>
2015-09-14 07:36:11 -0700

    More diagnostic output for asset scripts

--- ci/download-asset.sh
+++ ci/download-asset.sh
@@ -4,6 +4,9 @@ SUBTAG=$1
 DESTDIR=$2
 ZIPFILE=/tmp/$SUBTAG.zip
 
-curl -s -S "jsil.org/ci/download.aspx?key=travisci&tag=$JSIL_STORAGE_TAG-$SUBTAG&password=$JSIL_STORAGE_PASSWORD" -o $ZIPFILE
-unzip -q -o $ZIPFILE -d "$DESTDIR"
+echo "Downloading $SUBTAG..."
+curl -S "jsil.org/ci/download.aspx?key=travisci&tag=$JSIL_STORAGE_TAG-$SUBTAG&password=$JSIL_STORAGE_PASSWORD" -o "$ZIPFILE"
+ls -la "$ZIPFILE"
+echo "Unarchiving $SUBTAG..."
+unzip -q -o "$ZIPFILE" -d "$DESTDIR"
 rm -rf "$DESTDIR/$SUBTAG/$SUBTAG"
--- ci/upload-asset.sh
+++ ci/upload-asset.sh
@@ -4,8 +4,9 @@ SUBTAG=$1
 SOURCEDIR=$2
 ZIPFILE=/tmp/$SUBTAG.zip
 
+rm "$ZIPFILE"
+echo "Archiving $SUBTAG..."
+zip -3 -r -q "$ZIPFILE" "$SOURCEDIR"
 echo "Uploading $SUBTAG..."
-rm $ZIPFILE
-zip -3 -r -q $ZIPFILE "$SOURCEDIR"
 curl -S -X POST "jsil.org/ci/upload.aspx?key=travisci&tag=$JSIL_STORAGE_TAG-$SUBTAG&password=$JSIL_STORAGE_PASSWORD" --data-binary "@$ZIPFILE"
 echo "Done uploading $SUBTAG."
