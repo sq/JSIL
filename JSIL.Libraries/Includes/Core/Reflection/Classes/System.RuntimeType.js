25a86299c77dbb32361202616de75bca1e03eba1
ik <igor@kiselev.spb.ru>
2015-10-09 20:23:01 -0700

    RuntimeType.op_Equality implemented.

--- JSIL.Libraries/Includes/Core/Reflection/Classes/System.RuntimeType.js
+++ JSIL.Libraries/Includes/Core/Reflection/Classes/System.RuntimeType.js
@@ -1,3 +1,13 @@
 ﻿JSIL.MakeClass("System.Reflection.TypeInfo", "System.RuntimeType", false, [], function ($) {
   $jsilcore.RuntimeTypeInitialized = true;
+
+  $.Method({ Public: true, Static: true }, "op_Equality",
+    new JSIL.MethodSignature($.Boolean, [$.Type, $.Type]),
+    function(lhs, rhs) {
+      if (lhs === rhs)
+        return true;
+
+      return String(lhs) == String(rhs);
+    }
+  );
 });
