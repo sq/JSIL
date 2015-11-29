7743d6427d5b4e1fed3611c2efa1a4c6af3267e6
Michael Homer <mwh@ecs.vuw.ac.nz>
2015-11-30 10:19:24 +1300

    Implement String.Join(string, IEnumerable<string>)
    This String.Join overload was not declared, although JSIL.JoinEnumerable
    would already do the job if asked. This commit connects the two and
    updates the existing test to cover the IEnumerable<String> case as well.
    The other String.Join overloads are untouched.

--- Tests/SimpleTestCases/StringJoinEnumerable.cs
+++ Tests/SimpleTestCases/StringJoinEnumerable.cs
@@ -5,6 +5,9 @@ static class Program {
     public static void Main () {
         IEnumerable<A> items = new List<A>() { new A("Foo"), new A("Bar"), new A("Baz"), new A("Qux") };
         Console.WriteLine(string.Join("; ", items));
+
+        IEnumerable<string> strings = new List<string> { "Foo", "Bar", "Baz", "Qux" };
+        Console.WriteLine(string.Join("; ", strings));
     }
 }
 
