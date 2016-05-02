32b8894ce3ca963dc1e2b2dae61b67f69d8e32d1
Igor Kiselev <igor@kiselev.spb.ru>
2016-05-02 13:58:55 -0700

    NuGet packeges updated: Node.js/0.12.7 Npm/1.4.15.2 NoGit/0.0.8 FSharp.Compiler.CodeDom/1.0.0.1

--- JSIL.Libraries/.bin/node.cmd
+++ JSIL.Libraries/.bin/node.cmd
@@ -1,3 +1,3 @@
 @echo off
 SET PATH=%PATH%;%~dp0
-"%~dp0..\..\packages\Node.js.0.12.7\node.exe" %*
+"%~dp0..\..\packages\Node.js.5.3.0\node.exe" %*
--- JSIL.Libraries/.bin/npm.cmd
+++ JSIL.Libraries/.bin/npm.cmd
@@ -1,4 +1,4 @@
 @echo off
 SET PATH=%~dp0;%PATH%
 SET npm_config_git=%~dp0git.cmd
-"%~dp0node" "%~dp0..\..\packages\Npm.1.4.15.2\node_modules\npm\bin\npm-cli.js" %*
+"%~dp0node" "%~dp0..\..\packages\Npm.3.5.2\node_modules\npm\bin\npm-cli.js" %*
