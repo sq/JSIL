64e2ecc59bf2efb7bd518e4e711723a355aa5153
Igor Kiselev <igor@kiselev.spb.ru>
2016-06-08 16:44:59 -0700

    Tests updated according to new generation patterns

--- Tests/FormattingTests.cs
+++ Tests/FormattingTests.cs
@@ -467,8 +467,8 @@ namespace JSIL.Tests {
             );
 
             try {
-                Assert.IsTrue(generatedJs.Contains("Foo.$Methods.Func1.Of"), "Func1 was not called through the qualified method");
-                Assert.IsTrue(generatedJs.Contains("Foo.$Methods.Func2.Of"), "Func2 was not called through the qualified method");
+                Assert.IsTrue(generatedJs.Contains("Foo.$Methods.Func1.InterfaceMethod.Of"), "Func1 was not called through the qualified method");
+                Assert.IsTrue(generatedJs.Contains("Foo.$Methods.Func2.InterfaceMethod.Of"), "Func2 was not called through the qualified method");
                 Assert.IsTrue(generatedJs.Contains("this.Func2()"), "Func2 was not called through this");
                 Assert.IsTrue(generatedJs.Contains("this.Func2()"), "Func2 was not called through this");
 
@@ -604,9 +604,9 @@ namespace JSIL.Tests {
                     Assert.IsTrue(generatedJs.Contains("this.Test("), "this.Test was not direct-dispatched");
                     // FIXME: Is this right?
                     Assert.IsTrue(
-                        generatedJs.Contains("Interface.$Methods.Test2.Call(") ||
+                        generatedJs.Contains("Interface.$Methods.Test2.InterfaceMethod.Call(") ||
                         (
-                            generatedJs.ContainsRegex(@"\$IM([0-9]*) = JSIL.Memoize\(\$asm([0-9]*).Interface.\$Methods.Test2\)") &&
+                            generatedJs.ContainsRegex(@"\$IM([0-9]*) = JSIL.Memoize\(\$asm([0-9]*).Interface.\$Methods.Test2.InterfaceMethod\)") &&
                             generatedJs.ContainsRegex(@"\$IM([0-9]*)\(\).Call\(")
                         ), 
                     "test.Interface_Test2 was not direct-dispatched");
