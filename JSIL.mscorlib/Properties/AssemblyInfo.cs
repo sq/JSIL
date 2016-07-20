5a5a3aff573ac5b84fc50a23fb55eccf751c373e
Igor Kiselev <igor@kiselev.spb.ru>
2016-07-20 16:27:49 -0700

    Fixed AssemblyInfo

--- JSIL.mscorlib/Properties/AssemblyInfo.cs
+++ JSIL.mscorlib/Properties/AssemblyInfo.cs
@@ -9,10 +9,6 @@ using JSIL.Meta;
 [assembly: AssemblyTitle("JSIL.mscorlib")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("JSIL.mscorlib")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
-[assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
@@ -23,17 +19,5 @@ using JSIL.Meta;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("aef2633e-e92c-4a0d-b8e0-a072ed404af7")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
 [assembly: JSRepaceAssemblyDeclaration("mscorlib")]
 [assembly: JSOverrideAssemblyReference(typeof(object), "mscorlib")]
