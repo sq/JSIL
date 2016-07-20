0637214c6b00883218b8d4a088afc8f6950db2d6
Igor Kiselev <igor@kiselev.spb.ru>
2016-07-20 16:12:32 -0700

    Use JSIL\Properties\GlobalAssemblyInfo.cs for version-tracking attributes

--- Proxies/BCL/Properties/AssemblyInfo.cs
+++ Proxies/BCL/Properties/AssemblyInfo.cs
@@ -1,5 +1,4 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
@@ -8,10 +7,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("JSIL Proxies BLC")]
 [assembly: AssemblyDescription("JSIL Standard Type Proxies for BCL assemblies")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Squared Interactive")]
-[assembly: AssemblyProduct("JSIL")]
-[assembly: AssemblyCopyright("Copyright © Squared Interactive 2011")]
-[assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
@@ -21,16 +16,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("8BB79C50-0821-4C59-AFF8-21F25591F0E4")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
