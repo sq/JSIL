110706d4388e6babb1a16eb7408de36305dd531e
June Rhodes <jrhodes@redpointgames.com.au>
2016-09-19 22:59:33 +1000

    isolate npm

--- build_windows.bat
+++ build_windows.bat
@@ -4,6 +4,6 @@ git submodule update --init --recursive
 dir
 nuget.exe restore JSIL.sln
 cd packages\Npm.3.5.2\node_modules\npm
-npm install
+cmd /C "npm install"
 cd ..\..\..\..
 "C:\Program Files (x86)\MSBuild\14.0\Bin\amd64\msbuild.exe" "JSIL.sln" /m /verbosity:minimal /P:Platform=NoXNA
