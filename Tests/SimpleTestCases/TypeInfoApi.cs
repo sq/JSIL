bdbd230ce1cb346f2789159afa31db8ecd165c94
michael.daveynis <mdaveynis@users.noreply.github.com>
2017-03-14 19:41:18 -0700

    Update test to avoid System.Type.ToString() differences.

--- Tests/SimpleTestCases/TypeInfoApi.cs
+++ Tests/SimpleTestCases/TypeInfoApi.cs
@@ -4,8 +4,19 @@ using System.Linq;
 using System.Reflection;
 
 public static class Program {
-    public static void Main (string[] args) {
-        Console.WriteLine(IntrospectionExtensions.GetTypeInfo(typeof(Exception)).DeclaredConstructors.First(obj => obj.GetParameters().Length == 2));
+    public static void Main (string[] args)
+    {
+        var assertion = IntrospectionExtensions.GetTypeInfo(typeof(Exception)).DeclaredConstructors
+            .Any(obj => obj.GetParameters().Length == 2
+                        && obj.GetParameters()[0].ParameterType == typeof(string)
+                        && obj.GetParameters()[1].ParameterType == typeof(Exception));
+        if (!assertion)
+        {
+            throw new Exception("Invalid result for DeclaredConstructors");
+        }
+
+        Console.WriteLine(assertion ? "true" : "false");
+
         Console.WriteLine(typeof(List<int>).GetTypeInfo().GenericTypeArguments[0] == typeof(int) ? "true" : "false");
     }
 }
